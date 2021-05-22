d96e8e1376936713300a0e006307c1b21d99f540
deadit <stdeadit@gmail.com>
2021-05-22 10:13:41 +0300

    add wasm

---
+++ config-overrides.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = function override(config, env) {
+  /**
+   * Add WASM support
+   */
+
+  // Make file-loader ignore WASM files
+  const wasmExtensionRegExp = /\.wasm$/;
+  config.resolve.extensions.push('.wasm');
+  config.module.rules.forEach(rule => {
+      (rule.oneOf || []).forEach(oneOf => {
+          if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+              oneOf.exclude.push(wasmExtensionRegExp);
+          }
+      });
+  });
+
+  // Add a dedicated loader for WASM
+  config.module.rules.push({
+      test: wasmExtensionRegExp,
+      include: path.resolve(__dirname, 'src'),
+      use: [{ loader: require.resolve('wasm-loader'), options: {} }]
+  });
+
+  return config;
+};
