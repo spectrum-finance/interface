316dfe5a890e6c239d8a33b3e93efb72f644f681
deadit <stdeadit@gmail.com>
2021-06-22 16:23:00 +0300

    updates

--- src/components/Swap/Swap.tsx
+++ src/components/Swap/Swap.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import {
   AutoComplete,
   Button,
@@ -8,16 +8,28 @@ import {
   Select,
   Text,
 } from '@geist-ui/react';
+import { filter } from 'ramda';
 import { Form, Field, FieldRenderProps } from 'react-final-form';
-import { Explorer, NetworkPools, RustModule, T2tPoolOps } from 'ergo-dex-sdk';
+import { evaluate } from 'mathjs';
+import {
+  AmmPool,
+  Explorer,
+  NetworkPools,
+  RustModule,
+  T2tPoolOps,
+} from 'ergo-dex-sdk';
 import { YoroiProver } from '../../utils/yoroiProver';
 import {
+  AssetAmount,
+  AssetInfo,
   BoxSelection,
   DefaultBoxSelector,
   DefaultTransactions,
   DefaultTxAssembler,
 } from 'ergo-dex-sdk/build/module/ergo';
 import { fromAddress } from 'ergo-dex-sdk/build/module/ergo/entities/publicKey';
+import { useGetAllPools } from '../../hooks/useGetAllPools';
+import { getTokenInfo } from '../../utils/getTokenInfo';
 // import { fromAddress } from ;
 // import { T2tPoolOps } from 'ergo-dex-sdk';
 // import { NetworkPools } from 'ergo-dex-sdk';
@@ -29,31 +41,133 @@ import { fromAddress } from 'ergo-dex-sdk/build/module/ergo/entities/publicKey';
 interface Swap {
   isWalletConnected: boolean;
 }
-
+enum WalletStates {
+  NEED_TO_CONNECT_WALLET = 'NEED_TO_CONNECT_WALLET',
+  NEED_TO_CHOOSE_PAIR = 'NEED_TO_CHOOSE_PAIR',
+  NO_LIQUIDITY = 'NO_LIQUIDITY',
+  NO_AVAILABLE_POOL = 'NO_AVAILABLE_POOL',
+  NEED_TO_ENTER_AMOUNT = 'NEED_TO_ENTER_AMOUNT',
+  SUBMIT = 'SUBMIT',
+  LOADING = 'LOADING',
+}
 // function chooseBoxes(boxes: ErgoBox[]): BoxSelection;
+const getButtonState = ({
+  isWalletConnected,
+  choosedPool,
+  firstTokenId,
+  secondTokenId,
+  firstTokenAmount,
+  secondTokenAmount,
+}: any) => {
+  if (!isWalletConnected) {
+    return WalletStates.NEED_TO_CONNECT_WALLET;
+  }
 
-const getPoolByPair = (tokenId1: any, tokenId2: any) => {
-  return {
-    id: '12',
-    x: [],
-    y: [],
-  };
+  if (!firstTokenId || !secondTokenId) {
+    return WalletStates.NEED_TO_CHOOSE_PAIR;
+  }
+
+  if (!choosedPool) {
+    return WalletStates.NO_AVAILABLE_POOL;
+  }
+
+  if (!firstTokenAmount && !secondTokenAmount) {
+    // return WalletStates.NEED_TO_ENTER_AMOUNT;
+  }
+
+  if (choosedPool) {
+    return WalletStates.SUBMIT;
+  }
+
+  return WalletStates.LOADING;
 };
 
 export const Swap = ({ isWalletConnected }: Swap) => {
+  const [firstTokenId, setFirstTokenId] = useState('');
+  const [secondTokenId, setSecondTokenId] = useState('');
+  const [firstTokenAmount, setFirstTokenAmount] = useState('');
+  const [secondTokenAmount, setSecondTokenAmount] = useState('');
+  const [firstTokenInfo, setFirstTokenInfo] =
+    useState<AssetInfo | undefined>(undefined);
+  const [secondTokenInfo, setSecondTokenInfo] =
+    useState<AssetInfo | undefined>(undefined);
+  const pools = useGetAllPools();
+
+  const choosedPool = useMemo(() => {
+    if (!firstTokenId.trim() || !secondTokenId.trim()) {
+      return null;
+    }
+    return pools?.find(
+      (ammPool) =>
+        (ammPool.assetX.id === firstTokenId &&
+          ammPool.assetY.id === secondTokenId) ||
+        (ammPool.assetX.id === secondTokenId &&
+          ammPool.assetY.id === firstTokenId),
+    );
+  }, [firstTokenId, secondTokenId, pools]);
+
   const [addresses, setAddresses] = useState<string[]>([]);
   const [utxos, setUtxos] = useState([]);
-  const currencies = [
-    { label: 'ETH', value: 'ETH' },
-    { label: 'ERG', value: 'ERG' },
-  ]; //getPools { identifierPool: 12 }
 
-  const onSwap = useCallback(() => {
-    if (isWalletConnected) {
-      //
+  const buttonStatus = useMemo(() => {
+    console.log(choosedPool);
+    const buttonState = getButtonState({
+      isWalletConnected,
+      firstTokenId,
+      secondTokenId,
+      choosedPool,
+      firstTokenAmount,
+      secondTokenAmount,
+    });
+    switch (buttonState) {
+      case WalletStates.LOADING: {
+        return { disabled: true, text: 'Loading...' };
+      }
+      case WalletStates.NEED_TO_CHOOSE_PAIR: {
+        return { disabled: true, text: 'Need to choose pair' };
+      }
+      case WalletStates.NO_AVAILABLE_POOL: {
+        return { disabled: true, text: 'No available pool' };
+      }
+      case WalletStates.SUBMIT: {
+        return { disabled: false, text: 'Submit' };
+      }
+      case WalletStates.NEED_TO_CONNECT_WALLET: {
+        return { disabled: true, text: 'Need to connect wallet' };
+      }
+      // case WalletStates.NEED_TO_ENTER_AMOUNT: {
+      //   return { disabled: true, text: 'Need to enter amount' };
+      // }
     }
-    console.log(isWalletConnected);
-  }, [isWalletConnected]);
+  }, [isWalletConnected, firstTokenId, secondTokenId, choosedPool]);
+
+  const availableTokens: Record<string, AssetInfo> = useMemo(() => {
+    return (
+      pools?.reduce((acc, { assetX, assetY }) => {
+        acc[assetX.id] = { ...assetX };
+        acc[assetY.id] = { ...assetY };
+        return acc;
+      }, {} as any) || {}
+    );
+  }, [pools]);
+
+  const firstSelectTokens = useMemo(() => {
+    return Object.values(
+      filter((tokenData) => tokenData.id !== secondTokenId, availableTokens),
+    ).map((tokenData) => ({
+      label: tokenData.name || tokenData.id,
+      value: tokenData.id,
+    }));
+  }, [availableTokens, secondTokenId]);
+
+  const secondSelectTokens = useMemo(() => {
+    return Object.values(
+      filter((tokenData) => tokenData.id !== firstTokenId, availableTokens),
+    ).map((tokenData) => ({
+      label: tokenData.name || tokenData.id,
+      value: tokenData.id,
+    }));
+  }, [availableTokens, firstTokenId]);
 
   useEffect(() => {
     if (isWalletConnected) {
@@ -62,45 +176,80 @@ export const Swap = ({ isWalletConnected }: Swap) => {
     }
   }, [isWalletConnected]);
 
-  const onInputFirstAmount = () => ({});
+  const onSelectFirstToken = (value: any) => {
+    setFirstTokenId(value);
 
-  const onInputSecondAmount = () => ({});
+    getTokenInfo(value).then((data) => setFirstTokenInfo(data));
+  };
 
-  useEffect(() => {
-    if (isWalletConnected) {
-      ergo.get_used_addresses().then((data: string[]) => setAddresses(data));
-    }
-  }, [isWalletConnected]);
+  const onSelectSecondToken = (value: any) => {
+    setSecondTokenId(value);
+    getTokenInfo(value).then((data) => setSecondTokenInfo(data));
+  };
+
+  const onEnterFirstTokenAmount = (value: any) => {
+    setFirstTokenAmount(value);
+
+    // if (choosedPool && firstTokenInfo) {
+    //   const amount = choosedPool.outputAmount(
+    //     new AssetAmount(
+    //       firstTokenInfo,
+    //       BigInt(
+    //         evaluate(`${value}*10^${firstTokenInfo.decimals || 0}`).toFixed(0),
+    //       ),
+    //     ),
+    //     1,
+    //   );
+    //   setSecondTokenAmount(String(amount.amount));
+    // }
+  };
+
+  const onEnterSecondTokenAmount = (value: any) => {
+    setSecondTokenAmount(value);
+
+    // if (choosedPool) {
+    //   setFirstTokenAmount(value);
+    // }
+  };
 
   const onSubmit = async (values: any) => {
-    if (isWalletConnected) {
-      // const pk = fromAddress(values.address)!;
-      await RustModule.load();
+    if (isWalletConnected && choosedPool) {
       const network = new Explorer('https://api.ergoplatform.com');
       const poolNetwork = new NetworkPools(network);
       // // выбрать pool из селекта
       const pools = await poolNetwork.getAll({ limit: 100, offset: 0 });
       console.log(123, pools);
-      const chosenPool = pools[0][0];
-      const poolScriptHash = chosenPool.poolScriptHash;
+      const chosenPool = choosedPool;
+      const poolId = chosenPool.id;
       // const yoroiWalletProver = {} as any;
       const baseInput = chosenPool.x.withAmount(10n);
 
-      const poolOps = new DefaultTransactions(
+      const poolOps = new T2tPoolOps(
         new YoroiProver(),
         new DefaultTxAssembler(true),
       );
       const pk = fromAddress(addresses[0]) as string;
+      const minQuoteOutput = chosenPool.outputAmount(baseInput, 1).amount;
+      const dexFeePerToken = 100n;
+      const poolFeeNum = 600;
+      console.log(utxos);
       poolOps
-        .simple(
-          'PT6XSsf8tanNKVKyMzSBT5QfTFmdSgG2fPT84CCS818LxtayX4v6yR5RNpTybWHbjyQxdgPTzyM5NwkJ3oNeDDWXfS4tneegkRUfc83YryJ1p8Ax1w3jcpsFkmS83db1hwsWwnkqaW47VJ46YQvMSNquMVWxWVoPQoW5eVizsqyXRthWJwiEja51Qo91mULBCuRs9BAAtyXboXUb9NF3RKk4XU8ACfYuYndSUVDwqajgAeXKtN4J92dcTVJq7iQ4Pfa8iu9VVfDCda8kvPGmMwtr7jtCH2bNMQWAyGoad5yFUDiBKaXz2KP7vd3EjWBRU6WBsfBhNM2ZFp5kcfARXSaHH4x3zZnsguhzdR64zDxKsA9THvCmhDegLgfieAi1pCxjdAyTVThR2Hwx5w2RDKkb39i8MXJihkZto1EE9ic51LUnBjaHQreLoA88H9dcbwMqo8MZPyrut1x88qZz1UDCgAa2nWw4qtU3XuJBudGrGJjTKXpcyEywSz9k6NtStD',
+        .swap(
+          {
+            pk,
+            poolId,
+            baseInput,
+            minQuoteOutput,
+            dexFeePerToken,
+            quoteAsset: secondTokenId,
+            poolFeeNum,
+          },
           {
             inputs: DefaultBoxSelector.select(utxos, {
               nErgs: 20000000,
               assets: [
                 {
-                  tokenId:
-                    'f45c4f0d95ce1c64defa607d94717a9a30c00fdd44827504be20db19f4dce36f',
+                  tokenId: firstTokenId,
                   amount: 10,
                 },
               ],
@@ -138,9 +287,7 @@ export const Swap = ({ isWalletConnected }: Swap) => {
       // console.log(pk);
       //
     }
-    console.log(values);
   };
-
   return (
     <>
       <Card>
@@ -150,11 +297,11 @@ export const Swap = ({ isWalletConnected }: Swap) => {
             slippage: 1,
             firstTokenAmount: 0.0,
             secondTokenAmount: 0.0,
-            firstTokenName: 'ETH',
-            secondTokenName: 'ERG',
+            firstTokenName: '',
+            secondTokenName: '',
             address: '',
           }}
-          render={({ handleSubmit, form }) => (
+          render={({ handleSubmit, values }) => (
             <form onSubmit={handleSubmit}>
               <Grid.Container gap={1}>
                 {isWalletConnected && addresses.length !== 0 && (
@@ -203,13 +350,21 @@ export const Swap = ({ isWalletConnected }: Swap) => {
                 <Grid xs={6}>
                   <Field name="firstTokenName" component="select">
                     {(props: FieldRenderProps<string>) => (
-                      <AutoComplete
-                        disableFreeSolo
-                        options={currencies}
-                        initialValue="ETH"
+                      <Select
+                        placeholder="Token"
+                        style={{ minWidth: '80px' }}
                         {...props.input}
-                        onSelect={props.input.onChange}
-                      />
+                        onChange={(value) => {
+                          onSelectFirstToken(value as string);
+                          props.input.onChange(value);
+                        }}
+                      >
+                        {firstSelectTokens.map(({ value, label }) => (
+                          <Select.Option key={value} value={value}>
+                            {label || value}
+                          </Select.Option>
+                        ))}
+                      </Select>
                     )}
                   </Field>
                 </Grid>
@@ -220,7 +375,15 @@ export const Swap = ({ isWalletConnected }: Swap) => {
                         placeholder="0.0"
                         type="number"
                         width="100%"
+                        step="0.01"
+                        min="0"
+                        lang="en"
                         {...props.input}
+                        onChange={({ currentTarget }) => {
+                          const value = currentTarget.value;
+                          onEnterFirstTokenAmount(value);
+                          props.input.onChange(value);
+                        }}
                       />
                     )}
                   </Field>
@@ -231,13 +394,21 @@ export const Swap = ({ isWalletConnected }: Swap) => {
                 <Grid xs={6}>
                   <Field name="secondTokenName" component="select">
                     {(props: FieldRenderProps<string>) => (
-                      <AutoComplete
-                        disableFreeSolo
-                        options={currencies}
-                        initialValue="ERG"
+                      <Select
+                        placeholder="Token"
+                        style={{ minWidth: '80px' }}
                         {...props.input}
-                        onSelect={props.input.onChange}
-                      />
+                        onChange={(value) => {
+                          onSelectSecondToken(value as string);
+                          props.input.onChange(value);
+                        }}
+                      >
+                        {secondSelectTokens.map(({ value, label }) => (
+                          <Select.Option key={value} value={value}>
+                            {label || value}
+                          </Select.Option>
+                        ))}
+                      </Select>
                     )}
                   </Field>
                 </Grid>
@@ -249,13 +420,17 @@ export const Swap = ({ isWalletConnected }: Swap) => {
                         type="number"
                         width="100%"
                         {...props.input}
+                        onChange={(e) => {
+                          onEnterSecondTokenAmount(e.currentTarget.value);
+                          props.input.onChange(e.currentTarget.value);
+                        }}
                       />
                     )}
                   </Field>
                 </Grid>
                 <Grid xs={24} justify="center">
-                  <Button htmlType="submit">
-                    {isWalletConnected ? 'Swap' : 'Need to connect wallet'}
+                  <Button htmlType="submit" disabled={buttonStatus.disabled}>
+                    {buttonStatus.text}
                   </Button>
                 </Grid>
               </Grid.Container>
