6d7492aeca8f8bd16cf6da0390b2d0f525a647da
Brian Kent <bdkent@users.noreply.github.com>
2021-07-09 04:01:09 +0000

    typing fixes

--- src/utils/getTokenInfo.ts
+++ src/utils/getTokenInfo.ts
@@ -1,6 +1,9 @@
 import { Explorer } from 'ergo-dex-sdk';
+import { AssetInfo, TokenId } from 'ergo-dex-sdk/build/module/ergo';
 
-export const getTokenInfo = (tokenId: string) => {
+export const getTokenInfo = (
+  tokenId: TokenId,
+): Promise<AssetInfo | undefined> => {
   const network = new Explorer('https://api.ergoplatform.com');
 
   return network.getToken(tokenId);
