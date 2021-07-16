b60df9ccf375c06c1520313e22d247d7d666b7c5
deadit <stdeadit@gmail.com>
2021-07-16 12:17:47 +0300

    add history table

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,25 +1,13 @@
 import { Text, Button } from '@geist-ui/react';
-import React from 'react';
+import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faHistory } from '@fortawesome/free-solid-svg-icons';
 import { ConnectWallet } from '../../ConnectWallet/ConnectWallet';
 import css from './header.module.scss';
 import { SettingsModal } from '../../Settings/SettingsModal';
 import { useToggle } from '../../../hooks/useToggle';
-
-const Header = (): JSX.Element => {
-  return (
-    <header className={css.header}>
-      <Text h2 className={css.main}>
-        ErgoDEX
-      </Text>
-      <SettingsButton />
-      <ConnectWallet />
-    </header>
-  );
-};
-
-export default Header;
+import { WalletContext } from '../../../context/WalletContext';
+import { HistoryModal } from '../../History/HistoryModal';
 
 const SettingsButton = (): JSX.Element => {
   const [open, handleOpen, handleClose] = useToggle(false);
@@ -37,3 +25,37 @@ const SettingsButton = (): JSX.Element => {
     </div>
   );
 };
+
+const HistoryButton = (): JSX.Element => {
+  const [open, handleOpen, handleClose] = useToggle(false);
+
+  return (
+    <div>
+      <Button
+        auto
+        type="abort"
+        disabled={open}
+        icon={<FontAwesomeIcon icon={faHistory} size="lg" />}
+        onClick={handleOpen}
+      />
+      <HistoryModal open={open} onClose={handleClose} />
+    </div>
+  );
+};
+
+const Header = (): JSX.Element => {
+  const { isWalletConnected } = useContext(WalletContext);
+
+  return (
+    <header className={css.header}>
+      <Text h2 className={css.main}>
+        ErgoDEX
+      </Text>
+      <SettingsButton />
+      {isWalletConnected && <HistoryButton />}
+      <ConnectWallet />
+    </header>
+  );
+};
+
+export default Header;
