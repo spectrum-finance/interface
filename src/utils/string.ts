7a24ff8be9e4883c9a93210ddab8e1e54c9f6600
Alex Afanasiev <alex.afanasiev@geekbrains.ru>
2021-08-01 13:57:41 +0300

    refactor: refactor toFloat function

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,13 +1,18 @@
+const DOT_SYMBOL = '.';
+
 export const truncate = (id: string): string => {
   return `${id.slice(0, 16)}...${id.slice(48)}`;
 };
 
 export const toFloat = (value: string, maxDecimals?: number): string => {
+  if (value === DOT_SYMBOL) {
+    return '';
+  }
   // TODO: EDEX-230
   const result = value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');
 
   if (maxDecimals) {
-    const dotPosition = result.indexOf('.') + 1;
+    const dotPosition = result.indexOf(DOT_SYMBOL) + 1;
     return !dotPosition ? result : result.slice(0, dotPosition + maxDecimals);
   }
 
