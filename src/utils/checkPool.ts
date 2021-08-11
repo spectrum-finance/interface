1f4a6fa64a792e4dd2a3840940a21e1da586a085
oskin1 <ilya.arcadich@gmail.com>
2021-08-11 23:16:35 +0300

    Rollback logging.

--- src/utils/checkPool.ts
+++ src/utils/checkPool.ts
@@ -5,10 +5,8 @@ import {
 } from 'ergo-dex-sdk';
 import explorer from '../services/explorer';
 
-const v = new NetworkAmmPoolValidation(explorer);
+const poolValidation = new NetworkAmmPoolValidation(explorer);
 
 export const checkPool = async (pool: AmmPool): Promise<ValidationResult> => {
-  const r = await v.validate(pool);
-  console.log('r ', r);
-  return r;
+  return poolValidation.validate(pool);
 };
