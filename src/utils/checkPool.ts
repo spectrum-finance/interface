3249e71b1406494bf1ae3a6802eb565f86b40c8e
oskin1 <ilya.arcadich@gmail.com>
2021-08-11 21:25:16 +0300

    Pools validation fixed.

--- src/utils/checkPool.ts
+++ src/utils/checkPool.ts
@@ -5,6 +5,10 @@ import {
 } from 'ergo-dex-sdk';
 import explorer from '../services/explorer';
 
+const v = new NetworkAmmPoolValidation(explorer);
+
 export const checkPool = async (pool: AmmPool): Promise<ValidationResult> => {
-  return new NetworkAmmPoolValidation(explorer).validate(pool);
+  const r = await v.validate(pool);
+  console.log('r ', r);
+  return r;
 };
