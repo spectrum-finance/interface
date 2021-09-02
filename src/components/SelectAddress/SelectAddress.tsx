289350e92ebe43fa58382795e95eae02c0af1c52
yashablack <iamyashablack@gmail.com>
2021-09-03 00:45:45 +0300

    feat: add output address state manager

--- src/components/SelectAddress/SelectAddress.tsx
+++ src/components/SelectAddress/SelectAddress.tsx
@@ -4,15 +4,21 @@ import { OverflowAddress } from '../Settings/OverflowAddress';
 
 interface SelectAddressProps {
   addresses: string[];
+  selectedAddress?: string;
   onSelectAddress: (value: string | string[]) => void;
 }
 
 export const SelectAddress = ({
   addresses,
+  selectedAddress,
   onSelectAddress,
 }: SelectAddressProps): JSX.Element => {
   return (
-    <Select initialValue={addresses[0]} onChange={onSelectAddress} width="100%">
+    <Select
+      initialValue={selectedAddress || addresses[0]}
+      onChange={onSelectAddress}
+      width="100%"
+    >
       {addresses.map((address: string) => (
         <Select.Option key={address} value={address}>
           <OverflowAddress address={address} />
