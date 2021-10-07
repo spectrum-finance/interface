0a04c32790b0e866d536f227ca41e1e41409ff99
TrueLifeDev <truelife1228@outlook.com>
2021-10-07 11:29:07 -0500

    Wallet-connect-component issues fixed

--- src/core-components/ConnectWallet/ConnectWallet.stories.tsx
+++ src/core-components/ConnectWallet/ConnectWallet.stories.tsx
@@ -1,9 +1,10 @@
 import './ConnectWallet.stories.scss';
 
 import { Meta, Story } from '@storybook/react';
-import { Button, Col, Divider, Form, Row } from 'antd';
+import { Col, Row } from 'antd';
 import React from 'react';
 
+import { Button } from '../Button/Button';
 import ConnectWallet from './ConnectWallet';
 
 export default {
@@ -13,179 +14,68 @@ export default {
 
 export const Template: Story = () => (
   <div className="main-board">
-    <h1 style={{ marginBottom: 4 }}>Wallet Button</h1>
-    <p>Wallet status and balance information</p>
-    <h2 style={{ marginTop: 24 }}>When to use</h2>
-    <p style={{ marginBottom: 0 }}>If it is necessary to show:</p>
-    <ul style={{ marginBottom: 0 }}>
-      <li>Еhe address of the wallet.</li>
-      <li>Wallet balance.</li>
-      <li>Wallet status.</li>
-    </ul>
-    <p>
-      The Balnce only, “Address Only” and “Pending Icon” types are used in the
-      mobile phone adaptive.
-    </p>
+    <h1 style={{ marginBottom: 24 }}>Wallet Button</h1>
 
-    <Divider style={{ color: 'white' }} />
-
-    <h1>Type=Connect to Wallet</h1>
+    <h2>Type=Connect to Wallet</h2>
 
     <Row gutter={[{ xs: 8, sm: 16, md: 24 }, 8]}>
       <Col span={8}>
-        <h5>Default</h5>
         <Button className="connect-to-wallet">Connect to wallet</Button>
       </Col>
-      <Col span={8}>
-        <h5>Hover</h5>
-        <Button className="connect-to-wallet hover">Connect to wallet</Button>
-      </Col>
-      <Col span={8}>
-        <h5>Pressed</h5>
-        <Button className="connect-to-wallet pressed">Connect to wallet</Button>
-      </Col>
     </Row>
 
-    <h1>Type=Wallet Connected</h1>
+    <h2>Type=Wallet Connected</h2>
 
     <Row gutter={[{ xs: 8, sm: 16, md: 24 }, 8]}>
       <Col span={8}>
-        <h5>Default</h5>
         <ConnectWallet
-          type="wallet-connected"
-          balance={0}
-          address="0x088a...378a"
-        ></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Hover</h5>
-        <ConnectWallet
-          type="wallet-connected"
-          status="hover"
-          balance={0}
-          address="0x088a...378a"
-        ></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Pressed</h5>
-        <ConnectWallet
-          type="wallet-connected"
-          status="pressed"
+          type="default"
           balance={0}
           address="0x088a...378a"
         ></ConnectWallet>
       </Col>
     </Row>
 
-    <h1>Type=Balance Only</h1>
+    <h2>Type=Balance Only</h2>
 
     <Row gutter={[{ xs: 8, sm: 16, md: 24 }, 8]}>
       <Col span={8}>
-        <h5>Default</h5>
         <ConnectWallet type="balance-only" balance={0}></ConnectWallet>
       </Col>
-      <Col span={8}>
-        <h5>Hover</h5>
-        <ConnectWallet
-          type="balance-only"
-          status="hover"
-          balance={0}
-        ></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Pressed</h5>
-        <ConnectWallet
-          type="balance-only"
-          status="pressed"
-          balance={0}
-        ></ConnectWallet>
-      </Col>
     </Row>
 
-    <h1>Type=Address Only</h1>
+    <h2>Type=Address Only</h2>
 
     <Row gutter={[{ xs: 8, sm: 16, md: 24 }, 8]}>
       <Col span={8}>
-        <h5>Default</h5>
         <ConnectWallet
           type="address-only"
-          address="0x088a...378a"
-        ></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Hover</h5>
-        <ConnectWallet
-          type="address-only"
-          status="hover"
-          address="0x088a...378a"
-        ></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Pressed</h5>
-        <ConnectWallet
-          type="address-only"
-          status="pressed"
           address="0x088a...378a"
         ></ConnectWallet>
       </Col>
     </Row>
 
-    <h1>Type=Pending</h1>
+    <h2>Type=Pending</h2>
 
     <Row gutter={[{ xs: 8, sm: 16, md: 24 }, 8]}>
       <Col span={8}>
-        <h5>Default</h5>
         <ConnectWallet type="pending" balance={0}></ConnectWallet>
       </Col>
-      <Col span={8}>
-        <h5>Hover</h5>
-        <ConnectWallet
-          type="pending"
-          status="hover"
-          balance={0}
-        ></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Pressed</h5>
-        <ConnectWallet
-          type="pending"
-          status="pressed"
-          balance={0}
-        ></ConnectWallet>
-      </Col>
     </Row>
 
-    <h1>Type=Pending Text</h1>
+    <h2>Type=Pending Text</h2>
 
     <Row gutter={[{ xs: 8, sm: 16, md: 24 }, 8]}>
       <Col span={8}>
-        <h5>Default</h5>
         <ConnectWallet type="pending-text"></ConnectWallet>
       </Col>
-      <Col span={8}>
-        <h5>Hover</h5>
-        <ConnectWallet type="pending-text" status="hover"></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Pressed</h5>
-        <ConnectWallet type="pending-text" status="pressed"></ConnectWallet>
-      </Col>
     </Row>
 
-    <h1>Type=Pending Icon</h1>
+    <h2>Type=Pending Icon</h2>
 
     <Row gutter={[{ xs: 8, sm: 16, md: 24 }, 8]}>
       <Col span={8}>
-        <h5>Default</h5>
         <ConnectWallet type="pending-icon"></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Hover</h5>
-        <ConnectWallet type="pending-icon" status="hover"></ConnectWallet>
-      </Col>
-      <Col span={8}>
-        <h5>Pressed</h5>
-        <ConnectWallet type="pending-icon" status="pressed"></ConnectWallet>
       </Col>
     </Row>
   </div>
--- src/core-components/ConnectWallet/ConnectWallet.tsx
+++ src/core-components/ConnectWallet/ConnectWallet.tsx
@@ -5,38 +5,41 @@ import React from 'react';
 
 interface ConnectWalletProps {
   type:
-    | 'wallet-connected'
+    | 'default'
     | 'balance-only'
     | 'address-only'
     | 'pending'
     | 'pending-text'
     | 'pending-icon';
-  status?: 'default' | 'hover' | 'pressed';
   balance?: number;
   address?: string;
 }
 
 const ConnectWallet: React.FC<ConnectWalletProps> = ({
   type,
-  status = 'default',
   balance,
   address,
 }) => {
   const showBalance =
-    type === 'wallet-connected' ||
-    type === 'balance-only' ||
-    type === 'pending';
+    type === 'default' || type === 'balance-only' || type === 'pending';
 
   const pending =
     type === 'pending' || type === 'pending-text' || type === 'pending-icon';
 
   return (
-    <div className={`wrapper ${status}`}>
+    <div className="connect-wallet__wrapper">
       {showBalance && (
-        <span className={`balance-label ${type}`}>{balance} ERG</span>
+        <span
+          className={`connect-wallet__balance-label connect-wallet__${type}`}
+        >
+          {balance} ERG
+        </span>
       )}
       {type !== 'balance-only' && (
-        <Button className={`address-label ${type}`} loading={pending}>
+        <Button
+          className={`connect-wallet__address-label connect-wallet__${type}`}
+          loading={pending}
+        >
           {type === 'pending-icon' ? '' : pending ? `1 Pending` : address}
         </Button>
       )}
