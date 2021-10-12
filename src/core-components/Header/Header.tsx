fc7a0b0795f46af8a1a07502d5c7c7ee38156951
TrueLifeDev <truelife1228@outlook.com>
2021-10-12 10:05:46 -0500

    Header component start

---
+++ src/core-components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.scss';
+
+import React from 'react';
+
+// export interface Props {}
+
+export const Header: React.FC = () => {
+  return <div className="header-wrapper"></div>;
+};
