737006807025868ac5deb92c38231448c2c307b6
TrueLifeDev <truelife1228@outlook.com>
2021-10-12 10:18:46 -0500

    updated

--- src/core-components/Header/Header.tsx
+++ src/core-components/Header/Header.tsx
@@ -2,8 +2,29 @@ import './Header.scss';
 
 import React from 'react';
 
+import { Logo } from '../Logo/Logo';
+import { Tabs } from '../Tabs/Tabs';
+
 // export interface Props {}
 
 export const Header: React.FC = () => {
-  return <div className="header-wrapper"></div>;
+  return (
+    <div className="header-wrapper">
+      <Logo label></Logo>
+      <Tabs defaultActiveKey="1" type="card">
+        <Tabs.TabPane tab="Swap" key="1">
+          Swap content
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Pool" key="2">
+          Pool content
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Exchange" key="3">
+          Exchange content
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Disabled" key="4" disabled>
+          Disabled
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
 };
