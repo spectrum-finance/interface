b142adfca4fef25171024b61b3d20307bbb4e751
TrueLifeDev <truelife1228@outlook.com>
2021-10-13 11:04:53 -0500

    Header component updated

--- src/core-components/Header/Header.tsx
+++ src/core-components/Header/Header.tsx
@@ -1,8 +1,18 @@
 import './Header.scss';
 
-import { SettingOutlined } from '@ant-design/icons';
+import {
+  BarChartOutlined,
+  FileTextOutlined,
+  GithubOutlined,
+  GlobalOutlined,
+  InfoCircleOutlined,
+  QuestionCircleOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import { Switch } from 'antd';
 import React from 'react';
 
+import { ReactComponent as DarkModeOutlined } from '../../assets/icons/darkmode.svg';
 import { Button } from '../Button/Button';
 import { ConnectWallet } from '../ConnectWallet/ConnectWallet';
 import { Dropdown } from '../Dropdown/Dropdown';
@@ -11,6 +21,8 @@ import { Menu } from '../Menu/Menu';
 import { NetworkDropdown } from '../NetworkDropdown/NetworkDropdown';
 import { Tabs } from '../Tabs/Tabs';
 
+const { SubMenu } = Menu;
+
 const networks = [
   { name: 'ergo', token: 'erg-orange' },
   { name: 'cardano', token: 'ada' },
@@ -18,20 +30,40 @@ const networks = [
 
 const menuOthers = (
   <Menu>
-    <Menu.Item>
+    <Menu.Item icon={<InfoCircleOutlined />}>
       <a target="_blank" rel="noopener noreferrer">
-        1st menu item
+        About
       </a>
     </Menu.Item>
-    <Menu.Item>
+    <Menu.Item icon={<QuestionCircleOutlined />}>
       <a target="_blank" rel="noopener noreferrer">
-        2nd menu item
+        How to use
       </a>
     </Menu.Item>
-    <Menu.Item>
+    <Menu.Item icon={<FileTextOutlined />}>
       <a target="_blank" rel="noopener noreferrer">
-        3rd menu item
+        Docs
       </a>
+    </Menu.Item>
+    <Menu.Item icon={<GithubOutlined />}>
+      <a target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+    </Menu.Item>
+    <Menu.Item icon={<BarChartOutlined />}>
+      <a target="_blank" rel="noopener noreferrer">
+        Analytics
+      </a>
+    </Menu.Item>
+    <SubMenu icon={<GlobalOutlined />} title="Language">
+      <Menu.Item>English</Menu.Item>
+      <Menu.Item>Chinese</Menu.Item>
+    </SubMenu>
+    <Menu.Item icon={<DarkModeOutlined />}>
+      <a target="_blank" rel="noopener noreferrer">
+        Dark Mode
+      </a>
+      <Switch defaultChecked size="small" className="header_theme_switch" />
     </Menu.Item>
   </Menu>
 );
