f7959266f51e10e6d6d0bb4252f2312d87722fc0
yashablack <iamyashablack@gmail.com>
2021-10-23 23:16:52 +0300

    feat: add useBodyClass hook

---
+++ src/hooks/useBodyClass.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const addBodyClass = (className: string) =>
+  document.body.classList.add(className);
+const removeBodyClass = (className: string) =>
+  document.body.classList.remove(className);
+
+const useBodyClass = (className: string | Array<string>): void => {
+  useEffect(() => {
+    className instanceof Array
+      ? className.map(addBodyClass)
+      : addBodyClass(className);
+
+    return () => {
+      className instanceof Array
+        ? className.map(removeBodyClass)
+        : removeBodyClass(className);
+    };
+  }, [className]);
+};
+
+export { useBodyClass };
