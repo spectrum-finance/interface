00800fc1b8d0704fe0b06787aa39c91c278f29e5
yashablack <iamyashablack@gmail.com>
2021-10-24 11:47:19 +0300

    refactor: craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -11,17 +11,15 @@ module.exports = {
             javascriptEnabled: true,
           },
         },
-        modifyLessRule: (lessRule, context) => {
-          lessRule.use = lessRule.use.map(item => {
-            return !(item.loader || '').includes('resolve-url-loader') ?
-              item :
-              ({
-                ...item,
-                options: {...item.options, root: path.resolve(__dirname),},
-              })
+        modifyLessRule: (lessRule) => {
+          lessRule.use = lessRule.use.map((item) => {
+            return !(item.loader || '').includes('resolve-url-loader')
+              ? item
+              : {
+                  ...item,
+                  options: { ...item.options, root: path.resolve(__dirname) },
+                };
           });
-          // console.dir(JSON.stringify(lessRule, null, 2));
-          // console.dir(JSON.stringify(newLoaders, null, 2));
           return lessRule;
         },
       },
