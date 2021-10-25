f3af096fe2202dc32fd827ca3937f58aeee380f9
yashablack <iamyashablack@gmail.com>
2021-10-25 17:48:13 +0300

    feat: add cookie

--- src/utils/wallet/walletsOperations.ts
+++ src/utils/wallet/walletsOperations.ts
@@ -1,8 +1,10 @@
 import { WalletContextType } from '../../context';
+import { walletCookies } from '../cookies';
 
 export const connectYoroiWallet =
   (ctx: WalletContextType) => (): Promise<void> =>
     window
       .ergo_request_read_access()
       .then(ctx.setIsWalletConnected)
+      .then(() => walletCookies.setConnected())
       .catch((err) => console.error(err));
