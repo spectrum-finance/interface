9e48692f9f92ec8b803e35c001e53af17df10151
TrueLifeDev <truelife1228@outlook.com>
2021-10-25 12:20:34 -0500

    Wallet modal refactor

--- src/ergodex-cdk/components/Wallet/TokenListView.tsx
+++ src/ergodex-cdk/components/Wallet/TokenListView.tsx
@@ -2,20 +2,45 @@ import './TokenListView.less';
 
 import React from 'react';
 
-import { TokenListItem } from './TokenListItem';
+import { TokenIcon } from '../TokenIcon/TokenIcon';
 
-interface TokenListItem {
+interface TokenListItemProps {
+  symbol?: string;
+  name?: string;
+  iconName?: string;
+  balance?: number;
+}
+
+export const TokenListItem: React.FC<TokenListItemProps> = ({
+  symbol,
+  name,
+  iconName,
+  balance,
+}) => (
+  <div className="token-list-item-wrapper">
+    <TokenIcon name={iconName ?? symbol ?? 'empty'} />
+    <div className="token-name-balance">
+      <div className="token-symbol-name">
+        <span className="token-symbol">{symbol}</span>
+        <span className="token-name">{name}</span>
+      </div>
+      <span className="token-balance">{balance}</span>
+    </div>
+  </div>
+);
+
+interface TokenItem {
   symbol: string;
   name: string;
   iconName?: string;
 }
 interface TokenListViewProps {
-  tokenList: TokenListItem[];
+  tokenList: TokenItem[];
 }
 
 export const TokenListView: React.FC<TokenListViewProps> = ({ tokenList }) => {
   return (
-    <div className="token-list">
+    <div>
       {tokenList.map((token, key) => (
         <TokenListItem
           key={key}
