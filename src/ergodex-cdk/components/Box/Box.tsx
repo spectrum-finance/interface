40f219d51f7c048a791fbdc5431bd9e0b8196741
yashablack <iamyashablack@gmail.com>
2021-10-27 11:06:23 +0300

    refactor: update Box components

--- src/ergodex-cdk/components/Box/Box.tsx
+++ src/ergodex-cdk/components/Box/Box.tsx
@@ -6,16 +6,36 @@ import React from 'react';
 interface BoxProps extends React.PropsWithChildren<unknown> {
   borderRadius?: 's' | 'm' | 'l';
   isContrast?: boolean;
+  className?: string;
+  padding?: number | [number, number];
 }
 
-const Box = ({ children, borderRadius, isContrast }: BoxProps): JSX.Element => {
+const Box = ({
+  children,
+  className,
+  borderRadius,
+  isContrast,
+  padding,
+}: BoxProps): JSX.Element => {
+  const getPadding = (p: number | [number, number]) => {
+    if (p instanceof Array) {
+      return `calc(var(--ergo-base-gutter) / 2 * ${p[0]}) calc(var(--ergo-base-gutter) / 2 * ${p[1]})`;
+    }
+    return `calc(var(--ergo-base-gutter) / 2 * ${p})`;
+  };
   return (
     <div
       className={cn(
         'ergodex-box',
         borderRadius && `ergodex-box--radius-${borderRadius}`,
         isContrast && `ergodex-box--contrast`,
+        className,
       )}
+      style={{
+        padding: padding
+          ? getPadding(padding)
+          : `calc(var(--ergo-base-gutter) / 2)`,
+      }}
     >
       {children}
     </div>
