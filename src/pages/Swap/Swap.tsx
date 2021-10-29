72cd67e3f42ab4f5fea8380995e50b7f4f7cb67b
ridel1e <ridellee@gmail.com>
2021-10-29 20:46:44 +0300

    fix TokenControl.tsx

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -1,8 +1,12 @@
 import './Swap.less';
 
+import { Form } from 'antd';
 import React from 'react';
 
-import { TokenControl } from '../../components/TokenControl/TokenControl';
+import {
+  TokenControl,
+  TokenControlFormItem,
+} from '../../components/TokenControl/TokenControl';
 import {
   Box,
   Button,
@@ -100,34 +104,36 @@ export const Swap = () => {
   return (
     <Flex alignItems="center">
       <Box contrast padding={6} className="swap" borderRadius="l">
-        <Flex>
-          <Flex flexDirection="row" alignItems="center">
-            <Flex.Item flex={1}>
-              <Typography.Title level={4}>Swap</Typography.Title>
+        <Form>
+          <Flex>
+            <Flex flexDirection="row" alignItems="center">
+              <Flex.Item flex={1}>
+                <Typography.Title level={4}>Swap</Typography.Title>
+              </Flex.Item>
+              <Button size="large" type="text" icon={<SettingOutlined />} />
+              <Button size="large" type="text" icon={<HistoryOutlined />} />
+            </Flex>
+            <Flex.Item marginBottom={6}>
+              <Typography.Footnote>Ergo network</Typography.Footnote>
             </Flex.Item>
-            <Button size="large" type="text" icon={<SettingOutlined />} />
-            <Button size="large" type="text" icon={<HistoryOutlined />} />
+
+            <Flex.Item marginBottom={1}>
+              <TokenControlFormItem label="From" />
+            </Flex.Item>
+
+            <Flex.Item className="swap-button">
+              <Button icon={<SwapOutlined />} size="large" />
+            </Flex.Item>
+
+            <Flex.Item marginBottom={4}>
+              <TokenControlFormItem label="To" />
+            </Flex.Item>
+
+            <Button size="large" type="primary">
+              Select a token
+            </Button>
           </Flex>
-          <Flex.Item marginBottom={6}>
-            <Typography.Footnote>Ergo network</Typography.Footnote>
-          </Flex.Item>
-
-          <Flex.Item marginBottom={1}>
-            <TokenControl />
-          </Flex.Item>
-
-          <Flex.Item className="swap-button">
-            <Button icon={<SwapOutlined />} size="large" />
-          </Flex.Item>
-
-          <Flex.Item marginBottom={4}>
-            <TokenControl />
-          </Flex.Item>
-
-          <Button size="large" type="primary">
-            Select a token
-          </Button>
-        </Flex>
+        </Form>
       </Box>
     </Flex>
   );
