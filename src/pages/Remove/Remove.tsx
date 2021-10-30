92555ba6e9b0621da606eb70e76cb57e545a58c0
yashablack <iamyashablack@gmail.com>
2021-10-30 11:40:42 +0300

    feat: Remove page layout

--- src/pages/Remove/Remove.tsx
+++ src/pages/Remove/Remove.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FormPageWrapper } from '../../components/FormPageWrapper/FormPageWrapper';
+import { SubmitButton } from '../../components/SubmitButton/SubmitButton';
+import { TokenIcon } from '../../components/TokenIcon/TokenIcon';
 import { TokenIconPair } from '../../components/TokenIconPair/TokenIconPair';
 import {
   Box,
   Button,
   Flex,
   SettingOutlined,
+  Slider,
   Typography,
 } from '../../ergodex-cdk';
 
@@ -14,7 +17,134 @@ const mocks = {
   tokenPair: { tokenA: 'ERG', tokenB: 'SigUSD' },
 };
 
+const sliderMarks = {
+  0: 'Min',
+  25: '25%',
+  50: '50%',
+  75: '75%',
+  100: 'Max',
+};
+
+type RemovableAsset = {
+  name: string;
+  amount: number;
+  earnedFees: number;
+};
+
+type RemovableAssetPair = {
+  assetX: RemovableAsset;
+  assetY: RemovableAsset;
+};
+
+interface TokenSpaceProps {
+  title: string;
+  // assetPair: RemovableAssetPair;
+}
+
+const RemovePageSpaceWrapper: React.FC<{ title: string }> = ({
+  children,
+  title,
+}) => {
+  return (
+    <Flex flexDirection="col">
+      <Flex.Item marginBottom={2}>
+        <Typography.Title level={5}>{title}</Typography.Title>
+      </Flex.Item>
+      <Flex.Item>{children}</Flex.Item>
+    </Flex>
+  );
+};
+
+const TokenSpace: React.FC<TokenSpaceProps> = ({
+  title,
+  // assetPair,
+}): JSX.Element => {
+  return (
+    <RemovePageSpaceWrapper title={title}>
+      <Box contrast padding={4}>
+        <Flex flexDirection="col">
+          <Flex.Item marginBottom={2}>
+            <Flex justify="space-between" alignItems="center">
+              <Flex.Item>
+                <Flex alignItems="center">
+                  <Flex.Item marginRight={2}>
+                    <TokenIcon name="ERG" />
+                  </Flex.Item>
+                  <Flex.Item>
+                    <Typography.Title level={5}>ERG</Typography.Title>
+                  </Flex.Item>
+                </Flex>
+              </Flex.Item>
+              <Flex.Item>
+                <Flex>
+                  <Typography.Title level={5}>0,00003 ERG</Typography.Title>
+                </Flex>
+              </Flex.Item>
+            </Flex>
+          </Flex.Item>
+          <Flex.Item>
+            <Flex justify="space-between">
+              <Flex.Item>
+                <Flex>
+                  <Flex.Item marginRight={2}>
+                    <TokenIcon name="ERG" />
+                  </Flex.Item>
+                  <Flex.Item>
+                    <Typography.Title level={5}>ERG</Typography.Title>
+                  </Flex.Item>
+                </Flex>
+              </Flex.Item>
+              <Flex.Item>
+                <Flex>
+                  <Typography.Title level={5}>0,00003 ERG</Typography.Title>
+                </Flex>
+              </Flex.Item>
+            </Flex>
+          </Flex.Item>
+        </Flex>
+      </Box>
+    </RemovePageSpaceWrapper>
+  );
+};
+
+interface RemovePositionSliderProps {
+  percentage: number;
+  setPercentage: (p: number) => void;
+}
+
+const RemovePositionSlider: React.FC<RemovePositionSliderProps> = ({
+  percentage,
+  setPercentage,
+}) => {
+  return (
+    <Box contrast padding={4}>
+      <Flex flexDirection="col">
+        <Flex.Item>
+          <Flex flexDirection="col">
+            <Flex.Item marginBottom={4}>
+              <Flex alignItems="center" justify="center">
+                <Typography.Title level={1}>{percentage}%</Typography.Title>
+              </Flex>
+            </Flex.Item>
+            <Flex.Item>
+              <Slider
+                tooltipVisible={false}
+                marks={sliderMarks}
+                defaultValue={percentage}
+                onChange={setPercentage}
+              />
+            </Flex.Item>
+          </Flex>
+        </Flex.Item>
+      </Flex>
+    </Box>
+  );
+};
+
 const Remove = (): JSX.Element => {
+  const DEFAULT_SLIDER_PERCENTAGE = 50;
+  const [removePercent, setRemovePercent] = useState(DEFAULT_SLIDER_PERCENTAGE);
+
   return (
     <FormPageWrapper width={382}>
       <Flex flexDirection="col">
@@ -38,21 +168,24 @@ const Remove = (): JSX.Element => {
           </Flex>
         </Flex.Item>
         <Flex.Item marginBottom={4}>
-          <Typography.Title level={5}>Amount</Typography.Title>
-          <Box contrast>hello</Box>
+          <RemovePageSpaceWrapper title="Amount">
+            <RemovePositionSlider
+              percentage={removePercent}
+              setPercentage={setRemovePercent}
+            />
+          </RemovePageSpaceWrapper>
         </Flex.Item>
+
         <Flex.Item marginBottom={4}>
-          <Typography.Title level={5}>Pooled Assets</Typography.Title>
-          <Box contrast>hello</Box>
+          <TokenSpace title="Pooled Assets" />
         </Flex.Item>
+
         <Flex.Item marginBottom={4}>
-          <Typography.Title level={5}>Earned Fees</Typography.Title>
-          <Box contrast>hello</Box>
+          <TokenSpace title="Earned Fees" />
         </Flex.Item>
+
         <Flex.Item>
-          <Button type="primary" block>
-            Remove
-          </Button>
+          <SubmitButton>Remove</SubmitButton>
         </Flex.Item>
       </Flex>
     </FormPageWrapper>
