b0e486ba420caf396c7f022c908b16ce5741e1e0
ridel1e <ridellee@gmail.com>
2021-11-02 12:15:08 +0300

    add action form v1

---
+++ src/components/common/ActionForm/ActionForm.tsx
@@ -0,0 +1,83 @@
+import { Form, FormInstance } from 'antd';
+import React, { FC, ReactNode, useEffect, useState } from 'react';
+
+import { useWallet } from '../../../context';
+import { useConnection } from '../../../context/ConnectionContext';
+import { ActionButton, ActionButtonState } from './ActionButton/ActionButton';
+
+export interface ActionFormStrategy<T = any> {
+  isTokensNotSelected: (form: FormInstance<T>) => boolean;
+  isAmountNotEntered: (form: FormInstance<T>) => boolean;
+  getInsufficientTokenForTx: (form: FormInstance<T>) => undefined | string;
+  getInsufficientTokenForFee: (form: FormInstance<T>) => undefined | string;
+  request: (form: FormInstance<T>) => Promise<any>;
+  actionButtonCaption: () => ReactNode;
+}
+
+export interface ActionFormProps {
+  readonly form: FormInstance;
+  readonly strategy: ActionFormStrategy;
+  readonly children?: ReactNode | ReactNode[];
+  readonly initialValues?: any;
+}
+
+export const ActionForm: FC<ActionFormProps> = ({
+  form,
+  strategy,
+  children,
+  initialValues,
+}) => {
+  const { isWalletConnected, ergBalance } = useWallet();
+  const { online } = useConnection();
+  const [buttonData, setButtonData] = useState<{
+    state: ActionButtonState;
+    data?: any;
+  }>({
+    state: ActionButtonState.ACTION,
+    data: undefined,
+  });
+  const [formValueChanged, setFormValueChanged] = useState({});
+
+  useEffect(() => {
+    if (!isWalletConnected || !ergBalance) {
+      setButtonData({ state: ActionButtonState.CONNECT_WALLET });
+    } else if (!online) {
+      setButtonData({ state: ActionButtonState.CHECK_INTERNET_CONNECTION });
+    } else if (strategy.isTokensNotSelected(form)) {
+      setButtonData({ state: ActionButtonState.SELECT_TOKEN });
+    } else if (strategy.isAmountNotEntered(form)) {
+      setButtonData({ state: ActionButtonState.ENTER_AMOUNT });
+    } else if (strategy.getInsufficientTokenForTx(form)) {
+      setButtonData({
+        state: ActionButtonState.INSUFFICIENT_TOKEN_BALANCE,
+        data: { token: strategy.getInsufficientTokenForTx(form) },
+      });
+    } else if (strategy.getInsufficientTokenForFee(form)) {
+      setButtonData({
+        state: ActionButtonState.INSUFFICIENT_TOKEN_BALANCE,
+        data: { token: strategy.getInsufficientTokenForFee(form) },
+      });
+    }
+  }, [online, ergBalance, isWalletConnected, strategy, form, formValueChanged]);
+
+  //TODO: REWRITE
+  const onFormChange = () => setFormValueChanged({});
+
+  return (
+    <Form
+      form={form}
+      initialValues={initialValues}
+      onValuesChange={onFormChange}
+    >
+      {children}
+
+      <ActionButton
+        state={buttonData.state}
+        token={buttonData.data?.token}
+        nativeToken={buttonData.data?.nativeToken}
+      >
+        {strategy.actionButtonCaption()}
+      </ActionButton>
+    </Form>
+  );
+};
