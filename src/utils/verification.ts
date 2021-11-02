54896d4450f2804f90834f03a6387a65a2574c14
yashablack <iamyashablack@gmail.com>
2021-11-02 18:16:37 +0300

    refactor: refactor mappers

--- src/utils/verification.ts
+++ src/utils/verification.ts
@@ -1,17 +1,11 @@
 import { AmmPool } from '@ergolabs/ergo-dex-sdk';
 import { AssetInfo } from '@ergolabs/ergo-sdk/build/main/entities/assetInfo';
 
-import { TOKENS_MAPPER } from '../constants/mapper';
+import { VERIFIED_TOKENS } from '../constants/varifiedTokens';
 import { VERIFIED_POOLS } from '../constants/verifiedPools';
 
-export const isVerifiedToken = (token: AssetInfo): boolean => {
-  return Object.keys(TOKENS_MAPPER).some((key) => {
-    return key === token.name && TOKENS_MAPPER[key] === token.id;
-  });
-};
+export const isVerifiedToken = (token: AssetInfo): boolean =>
+  VERIFIED_TOKENS.has(token.id);
 
-export const isVerifiedPool = (pool: AmmPool): boolean => {
-  return Object.values(VERIFIED_POOLS).some((poolId) => {
-    return poolId === pool.id;
-  });
-};
+export const isVerifiedPool = (pool: AmmPool): boolean =>
+  VERIFIED_POOLS.has(pool.id);
