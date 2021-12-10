65864e8683d3d87ba6ebd9d3d084274b6e9a2364
Denis Kovalev <deniskovalev4791@gmail.com>
2021-12-11 00:06:59 +0300

    Add sentry integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,21 @@ import './assets/styles/styles.less';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import { App } from './App';
 import { reportWebVitals } from './reportWebVitals';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
