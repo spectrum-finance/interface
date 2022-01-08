222c4ee5b9a0cebb7aa5e3e4485dd247b046e9cc
yashablack <iamyashablack@gmail.com>
2022-01-08 18:15:01 +0300

    refactor: rewrite usePosition hook with all pools data

--- src/hooks/usePosition.ts
+++ src/hooks/usePosition.ts
@@ -1,10 +1,13 @@
-import { AmmPool, PoolId } from '@ergolabs/ergo-dex-sdk';
+import { PoolId } from '@ergolabs/ergo-dex-sdk';
 
-import { useNetworkPools } from './useNetworkPools';
+import { AmmPool } from '../common/models/AmmPool';
+// import { useNetworkPools } from './useNetworkPools';
+import { useGetAllPools } from './useGetAllPools';
 
 const usePosition = (poolId: PoolId): AmmPool | undefined => {
-  const positions = useNetworkPools();
-  return positions?.find((position) => position.id === poolId);
+  const positions = useGetAllPools();
+  const pl = positions?.find((position) => position.id === poolId);
+  return pl ? new AmmPool(pl) : undefined;
 };
 
 export { usePosition };
