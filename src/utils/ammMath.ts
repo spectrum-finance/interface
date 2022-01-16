15aabfcfb7f680ff8e2a4da410e95a8c0847a8a1
yashablack <iamyashablack@gmail.com>
2022-01-16 16:20:33 +0300

    refactor: rename makeTarget argument.

--- src/utils/ammMath.ts
+++ src/utils/ammMath.ts
@@ -2,14 +2,14 @@ import { AssetAmount, isNative, OverallAmount } from '@ergolabs/ergo-sdk';
 
 export function makeTarget(
   assetsIn: AssetAmount[],
-  minFeeForOrder: bigint,
+  minNErgForFee: bigint,
 ): OverallAmount {
   const nativeAsset = assetsIn.filter((i) => isNative(i.asset))[0];
   const isNativePool = !!nativeAsset;
 
   const totalNErgs = isNativePool
-    ? nativeAsset.amount + minFeeForOrder
-    : minFeeForOrder;
+    ? nativeAsset.amount + minNErgForFee
+    : minNErgForFee;
 
   const assets = assetsIn
     .filter((i) => !isNative(i.asset))
