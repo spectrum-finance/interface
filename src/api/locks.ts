8f36ffb69784032bda911d7abe15c11905df5059
ridel1e <ridellee@gmail.com>
2022-01-23 16:40:44 +0300

    add locks stream to api

---
+++ src/api/locks.ts
@@ -0,0 +1,20 @@
+import { TokenLock } from '@ergolabs/ergo-dex-sdk/build/main/security/entities';
+import { map, Observable, publishReplay, refCount, switchMap } from 'rxjs';
+
+import { AmmPool } from '../common/models/AmmPool';
+import { selectedNetwork$ } from '../network/network';
+
+export const locks$ = selectedNetwork$.pipe(
+  switchMap((network) => network.locks$),
+  publishReplay(1),
+  refCount(),
+);
+
+export const getLockByPool = (
+  pool: AmmPool,
+): Observable<TokenLock | undefined> =>
+  locks$.pipe(
+    map((locks) =>
+      locks.find((l) => l.lockedAsset.asset.id === pool.lp.asset.id),
+    ),
+  );
