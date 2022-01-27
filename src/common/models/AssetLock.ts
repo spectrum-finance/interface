b3b37eb1eaccbf7a7ab66f805d9fbbe5fab69631
ridel1e <ridellee@gmail.com>
2022-01-27 21:21:29 +0300

    add cache to base models

--- src/common/models/AssetLock.ts
+++ src/common/models/AssetLock.ts
@@ -1,6 +1,7 @@
 import { blocksToMillis, millisToBlocks } from '@ergolabs/ergo-dex-sdk';
 import { TokenLock } from '@ergolabs/ergo-dex-sdk/build/main/security/entities';
 import { BoxId } from '@ergolabs/ergo-sdk';
+import { cache } from 'decorator-cache-getter';
 import { DateTime } from 'luxon';
 
 import { AmmPool } from './AmmPool';
@@ -12,14 +13,17 @@ export enum AssetLockStatus {
 }
 
 export class AssetLock {
+  @cache
   get deadline(): number {
     return this.tokenLock.deadline;
   }
 
+  @cache
   get active(): boolean {
     return this.tokenLock.active;
   }
 
+  @cache
   get lp(): Currency {
     return new Currency(
       this.tokenLock.lockedAsset.amount,
@@ -27,18 +31,21 @@ export class AssetLock {
     );
   }
 
+  @cache
   get x(): Currency {
     const [x] = this.pool.shares(this.lp);
 
     return x;
   }
 
+  @cache
   get y(): Currency {
     const [, y] = this.pool.shares(this.lp);
 
     return y;
   }
 
+  @cache
   get status(): AssetLockStatus {
     if (this.currentBlock <= this.deadline) {
       return AssetLockStatus.LOCKED;
@@ -46,6 +53,7 @@ export class AssetLock {
     return AssetLockStatus.UNLOCKED;
   }
 
+  @cache
   get unlockDate(): DateTime {
     return DateTime.now().plus({
       millisecond: Number(
@@ -54,10 +62,12 @@ export class AssetLock {
     });
   }
 
+  @cache
   get boxId(): BoxId {
     return this.tokenLock.boxId;
   }
 
+  @cache
   get redeemer(): string {
     return this.tokenLock.redeemer;
   }
