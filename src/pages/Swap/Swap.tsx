7909f1f265864c09b551d05ba3720d2a246b91ca
ridel1e <ridellee@gmail.com>
2022-02-11 14:38:21 +0300

    add min value validation to liquidity. Optimize swap recalculation stream & fix currency math methods

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -10,7 +10,6 @@ import {
   combineLatest,
   debounceTime,
   distinctUntilChanged,
-  filter,
   map,
   Observable,
   of,
@@ -139,7 +138,8 @@ export const Swap = (): JSX.Element => {
     }
 
     if (!fromAmount?.isPositive() && toAmount?.isPositive() && pool) {
-      return pool.calculateOutputAmount(new Currency(1n, fromAsset));
+      // TODO: FIX_ERGOLABS_SDK_COMPUTING
+      return pool.calculateOutputAmount(new Currency(1n, fromAsset)).plus(1n);
     }
     if (!toAmount?.isPositive() && fromAmount?.isPositive() && pool) {
       return pool.calculateInputAmount(new Currency(1n, toAsset));
@@ -187,11 +187,9 @@ export const Swap = (): JSX.Element => {
   useSubscription(
     combineLatest([
       form.controls.fromAsset.valueChangesWithSilent$.pipe(
-        filter(Boolean),
         distinctUntilChanged(),
       ),
       form.controls.toAsset.valueChangesWithSilent$.pipe(
-        filter(Boolean),
         distinctUntilChanged(),
       ),
     ]).pipe(
@@ -255,11 +253,7 @@ export const Swap = (): JSX.Element => {
   );
 
   useSubscription(
-    combineLatest([
-      form.controls.toAsset.valueChanges$,
-      form.controls.fromAsset.valueChanges$,
-      form.controls.pool.valueChanges$,
-    ]).pipe(debounceTime(200)),
+    form.controls.pool.valueChanges$,
     () => {
       const { fromAmount, toAmount, pool } = form.value;
 
