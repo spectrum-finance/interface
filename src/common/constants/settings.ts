e27a6e447c7eab8c2c6ed6aa4e409bd3515f06ed
ridel1e <ridellee@gmail.com>
2022-02-18 14:22:26 +0300

    fix slippage min value

--- src/common/constants/settings.ts
+++ src/common/constants/settings.ts
@@ -7,7 +7,7 @@ export const MinerFeeMax = 5;
 export const MinerFeeDecimals = 2;
 
 export const defaultSlippage = 3;
-export const SlippageMin = 0.1;
+export const SlippageMin = 0.01;
 export const SlippageMax = 100;
 export const SlippageDecimals = 2;
 
