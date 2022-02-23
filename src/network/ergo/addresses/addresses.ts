6764e98264d04ada26133fb545c30916f56d0d5a
ridel1e <ridellee@gmail.com>
2022-02-23 10:00:57 +0300

    fix addresses stream

--- src/network/ergo/addresses/addresses.ts
+++ src/network/ergo/addresses/addresses.ts
@@ -1,12 +1,4 @@
-import {
-  filter,
-  map,
-  Observable,
-  publishReplay,
-  refCount,
-  switchMap,
-  zip,
-} from 'rxjs';
+import { filter, Observable, publishReplay, refCount, switchMap } from 'rxjs';
 
 import { appTick$ } from '../../../common/streams/appTick';
 import { WalletState } from '../../common';
@@ -15,15 +7,8 @@ import { selectedWallet$, selectedWalletState$ } from '../wallets';
 export const getAddresses = (): Observable<string[]> =>
   selectedWalletState$.pipe(
     filter((state) => state === WalletState.CONNECTED),
-    switchMap(() =>
-      selectedWallet$.pipe(
-        filter(Boolean),
-        switchMap((wallet) =>
-          zip(wallet.getUsedAddresses(), wallet.getUnusedAddresses()),
-        ),
-      ),
-    ),
-    map(([usedAddrs, unusedAddrs]) => unusedAddrs.concat(usedAddrs)),
+    switchMap(() => selectedWallet$.pipe(filter(Boolean))),
+    switchMap((wallet) => wallet.getAddresses()),
   );
 
 export const addresses$: Observable<string[]> = appTick$.pipe(
