3c7849fe2919f1ac5ae80b742e01750a5792219b
ridel1e <ridellee@gmail.com>
2022-02-23 10:11:04 +0300

    update prover methods

--- src/services/yoroi/index.ts
+++ src/services/yoroi/index.ts
@@ -2,9 +2,15 @@ import { Address, ErgoTx, ergoTxToProxy } from '@ergolabs/ergo-sdk';
 import { Balance } from '@ergolabs/ergo-sdk/build/main/wallet/entities/balance';
 
 import { explorer } from '../explorer';
-
+// TODO: WALLET_REFACTORING
 export const submitTx = async (tx: ErgoTx): Promise<any> => {
-  return await ergo.submit_tx(ergoTxToProxy(tx));
+  try {
+    return await ergo.submit_tx(ergoTxToProxy(tx));
+  } catch {
+    return ergoConnector.nautilus
+      .getContext()
+      .then((context) => context.submit_tx(ergoTxToProxy(tx)));
+  }
 };
 
 export const getBalance = async (
