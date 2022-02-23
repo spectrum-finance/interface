4f2f73084200b7e18a542d8981d5d655086e031a
ridel1e <ridellee@gmail.com>
2022-02-23 10:11:32 +0300

    update prover methods

--- src/services/yoroi/prover.ts
+++ src/services/yoroi/prover.ts
@@ -7,20 +7,36 @@ import {
   unsignedErgoTxToProxy,
 } from '@ergolabs/ergo-sdk';
 
+// TODO: WALLET_REFACTORING
 class YoroiProver implements Prover {
   /** Sign the given transaction.
    */
   async sign(tx: UnsignedErgoTx): Promise<ErgoTx> {
     const proxy = unsignedErgoTxToProxy(tx);
-    const res = await ergo.sign_tx(proxy);
-    return ergoTxFromProxy(res);
+
+    try {
+      const res = await ergo.sign_tx(proxy);
+      return ergoTxFromProxy(res);
+    } catch {
+      const res = await ergoConnector.nautilus
+        .getContext()
+        .then((context) => context.sign_tx(proxy));
+      return ergoTxFromProxy(res);
+    }
   }
 
   /** Sign particular input of the given transaction.
    */
   signInput(tx: UnsignedErgoTx, input: number): Promise<TxInput> {
     const proxy = unsignedErgoTxToProxy(tx);
-    return ergo.sign_tx_input(proxy, input);
+
+    try {
+      return ergo.sign_tx_input(proxy, input);
+    } catch {
+      return ergoConnector.nautilus
+        .getContext()
+        .then((context) => context.sign_tx_input(proxy, input));
+    }
   }
 }
 
