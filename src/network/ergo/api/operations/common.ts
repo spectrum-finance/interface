6befc89e93e29919b59270dfdac2bad1ab05e282
yashablack <iamyashablack@gmail.com>
2022-04-21 19:32:49 +0300

    refactor: ergo operations

--- src/network/ergo/api/operations/common.ts
+++
@@ -1,9 +0,0 @@
-import { makeDefaultPoolActionsSelector } from '@ergolabs/ergo-dex-sdk';
-import { ErgoTx } from '@ergolabs/ergo-sdk';
-import { filter, first, Observable, switchMap } from 'rxjs';
-
-import { UI_REWARD_ADDRESS } from '../../../../common/constants/settings';
-import { TxId } from '../../../../common/types';
-import { mainnetTxAssembler } from '../../../../services/defaultTxAssembler';
-import { selectedWallet$ } from '../wallet/wallet';
-import { proverMediator } from './common/proverMediator';
