c7c5211bad38c70308047f6b06c2c452cdcf1d06
ridel1e <ridellee@gmail.com>
2022-05-01 07:35:56 +0300

    add metadata url

--- src/applicationConfig.ts
+++ src/applicationConfig.ts
@@ -40,7 +40,7 @@ export const applicationConfig: ApplicationConfig = {
   requestRetryCount: 3,
   networksSettings: {
     cardano: {
-      metadataUrl: '',
+      metadataUrl: 'http://176.9.9.198:8080/metadata',
       networkUrl: 'https://testnet-api.quickblue.io/v1',
       explorerUrl: 'https://testnet.cardanoscan.io',
       faucet: 'https://faucet.ergodex.io/v1/',
