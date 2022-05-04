1d6227378f3e6a4a0f985a36a65eadbcde63957c
ridel1e <ridellee@gmail.com>
2022-05-04 17:55:19 +0300

    remove address tab from cardano network

--- src/components/WalletModal/WalletModal.tsx
+++ src/components/WalletModal/WalletModal.tsx
@@ -8,6 +8,8 @@ import { networkAssetBalance$ } from '../../gateway/api/networkAssetBalance';
 import { useSelectedNetwork } from '../../gateway/common/network';
 import { isLowBalance } from '../../utils/walletMath';
 import { ChooseWalletModal } from '../common/ConnectWalletButton/ChooseWalletModal/ChooseWalletModal';
+import { IsCardano } from '../IsCardano/IsCardano';
+import { IsErgo } from '../IsErgo/IsErgo';
 import { AddressesTab } from './AddressesTab/AddressesTab';
 import { LowBalanceWarning } from './LowBalanceWarning/LowBalanceWarning';
 import { TokensTab } from './TokensTab/TokensTab';
@@ -42,18 +44,25 @@ export const WalletModal: React.FC = () => {
           </Flex.Item>
           <Flex.Item marginBottom={6}>
             <Box contrast padding={4} borderRadius="m">
-              <Tabs defaultActiveKey="1" centered>
-                <Tabs.TabPane tab="Addresses" key="1">
-                  <Box transparent padding={[4, 0, 0, 0]} bordered={false}>
-                    <AddressesTab />
-                  </Box>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Tokens" key="2">
-                  <Box transparent padding={[4, 0, 0, 0]} bordered={false}>
-                    <TokensTab />
-                  </Box>
-                </Tabs.TabPane>
-              </Tabs>
+              <IsErgo>
+                <Tabs defaultActiveKey="1" centered>
+                  <Tabs.TabPane tab="Addresses" key="1">
+                    <Box transparent padding={[4, 0, 0, 0]} bordered={false}>
+                      <AddressesTab />
+                    </Box>
+                  </Tabs.TabPane>
+                  <Tabs.TabPane tab="Tokens" key="2">
+                    <Box transparent padding={[4, 0, 0, 0]} bordered={false}>
+                      <TokensTab />
+                    </Box>
+                  </Tabs.TabPane>
+                </Tabs>
+              </IsErgo>
+              <IsCardano>
+                <Box transparent padding={0} bordered={false}>
+                  <TokensTab />
+                </Box>
+              </IsCardano>
             </Box>
           </Flex.Item>
           <Button type="default" size="large" onClick={openChooseWalletModal}>
