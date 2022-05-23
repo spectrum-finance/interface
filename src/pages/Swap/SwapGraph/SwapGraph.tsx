23a982b1d6ed95b94b1e8b0388682847e1487101
Sergey Koshechkin <serge.koshechkin@gmail.com>
2022-05-23 16:28:17 +0300

    add last

--- src/pages/Swap/SwapGraph/SwapGraph.tsx
+++ src/pages/Swap/SwapGraph/SwapGraph.tsx
@@ -64,12 +64,14 @@ export const SwapGraph: React.FC<SwapGraphProps> = ({ pool }) => {
     }
 
     let j = -1;
-    return ticks.map((lts: DateTime) => {
+    const res = ticks.map((lts: DateTime) => {
       while (rawData[j + 1]?.ts < lts.valueOf()) {
         j++;
       }
       return rawData[j === -1 ? 0 : j].clone({ timestamp: lts.valueOf() });
     });
+    res.push(rawData[rawData.length - 1]);
+    return res;
   }, [rawData]);
 
   const [activeData, setActiveData] = useActiveData(data);
