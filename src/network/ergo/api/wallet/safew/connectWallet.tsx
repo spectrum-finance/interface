1aec93c2fa8ab701ab5cf2e5c29bebfb4d139bac
ThierryM1212 <thierry.mangiante@gmail.com>
2022-05-29 19:26:50 +0200

    wait for SAFEW ergoConnector to be loaded before using it

--- src/network/ergo/api/wallet/safew/connectWallet.tsx
+++ src/network/ergo/api/wallet/safew/connectWallet.tsx
@@ -1,6 +1,15 @@
 import { catchError, from, Observable, of, throwError } from 'rxjs';
 
 export const connectWallet = (): Observable<any> => {
+  // Wait 0.2s to be sure the ergoConnector is loaded in the page
+  // With mv3, there is no guarantee for it to be loaded before the page
+  const start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > 200) {
+      break;
+    }
+  }
+
   if (!ergoConnector?.safew) {
     return throwError(() => new Error('EXTENSION_NOT_FOUND'));
   }
