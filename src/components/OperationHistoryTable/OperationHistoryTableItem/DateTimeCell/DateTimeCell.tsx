a1d758c5e8276f9dd10a4d8d5ae0fc5ca0d77d14
ridel1e <ridellee@gmail.com>
2022-05-31 12:34:15 +0300

    fix history button

--- src/components/OperationHistoryTable/OperationHistoryTableItem/DateTimeCell/DateTimeCell.tsx
+++ src/components/OperationHistoryTable/OperationHistoryTableItem/DateTimeCell/DateTimeCell.tsx
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon';
 import React, { FC } from 'react';
 
-import { Flex } from '../../../../ergodex-cdk';
+import { Flex, Typography } from '../../../../ergodex-cdk';
 import { DateTimeView } from '../../../common/DateTimeView/DateTimeView';
 
 export interface DateTimeCellProps {
@@ -9,10 +9,12 @@ export interface DateTimeCellProps {
 }
 
 export const DateTimeCell: FC<DateTimeCellProps> = ({ dateTime }) => (
-  <Flex col>
-    <Flex.Item marginBottom={1}>
-      <DateTimeView value={dateTime} />
-    </Flex.Item>
-    <DateTimeView type="time" value={dateTime} />
-  </Flex>
+  <Typography.Body>
+    <Flex col>
+      <Flex.Item marginBottom={1}>
+        <DateTimeView value={dateTime} />
+      </Flex.Item>
+      <DateTimeView type="time" value={dateTime} />
+    </Flex>
+  </Typography.Body>
 );
