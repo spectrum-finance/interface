9170b8d0fa656468b984fe4881c069b5225d467b
Sergey Koshechkin <serge.koshechkin@gmail.com>
2022-06-15 14:06:54 +0300

    remove react import

--- src/ergodex-cdk/components/Modal/ModalContent/ModalContent.tsx
+++ src/ergodex-cdk/components/Modal/ModalContent/ModalContent.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import { CSSProperties } from 'react';
 import styled from 'styled-components';
 
 export const ModalContent = styled.div<{ width?: CSSProperties['width'] }>`
