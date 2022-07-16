3c69e389f5132a3eae5d6826d93893914c4f25f7
ridel1e <ridellee@gmail.com>
2022-07-16 16:42:38 +0300

    update AssetListItem.tsx

--- src/components/common/TokenControl/AssetSelect/AssetListModal/AssetListSelectTokenState/AssetListItem/AssetListItem.tsx
+++ src/components/common/TokenControl/AssetSelect/AssetListModal/AssetListSelectTokenState/AssetListItem/AssetListItem.tsx
@@ -1,10 +1,12 @@
-import { AssetInfo } from '@ergolabs/ergo-sdk/build/main/entities/assetInfo';
 import { Box, Flex, Typography } from '@ergolabs/ui-kit';
 import React from 'react';
 import styled from 'styled-components';
 
+import { AssetInfo } from '../../../../../../../common/models/AssetInfo';
 import { useAssetsBalance } from '../../../../../../../gateway/api/assetBalance';
-import { AssetTitle } from '../../../../../../AssetTitle/AssetTitle';
+import { AssetIcon } from '../../../../../../AssetIcon/AssetIcon';
+import { ConvenientAssetView } from '../../../../../../ConvenientAssetView/ConvenientAssetView';
+import { Truncate } from '../../../../../../Truncate/Truncate';
 
 interface TokenListItemProps {
   asset: AssetInfo;
@@ -12,6 +14,11 @@ interface TokenListItemProps {
   height?: number;
   onClick?: React.MouseEventHandler<HTMLElement>;
 }
+
+const StyledFootnote = styled(Typography.Footnote)`
+  font-size: 10px;
+  line-height: 15px;
+`;
 
 const _AssetListItem: React.FC<TokenListItemProps> = ({
   asset,
@@ -27,15 +34,32 @@ const _AssetListItem: React.FC<TokenListItemProps> = ({
       className={className}
       onClick={onClick}
       borderRadius="m"
-      padding={[3, 4]}
+      padding={[0, 4]}
       bordered={false}
     >
       <Flex align="center" width="100%">
-        <AssetTitle asset={asset} size="large" level={4} gap={2} />
-        <Flex.Item flex={1} justify="flex-end">
+        <Flex.Item marginRight={2} align="center">
+          <AssetIcon asset={asset} size="large" />
+        </Flex.Item>
+        <Flex.Item col display="flex" justify="center">
+          <Typography.Title level={4}>
+            <Truncate limit={20}>{asset.ticker}</Truncate>
+          </Typography.Title>
+          <StyledFootnote>{asset.name}</StyledFootnote>
+        </Flex.Item>
+        <Flex.Item
+          flex={1}
+          align="flex-end"
+          col
+          display="flex"
+          justify="center"
+        >
           <Typography.Title level={5}>
             {balance.get(asset).toString()}
           </Typography.Title>
+          <StyledFootnote>
+            <ConvenientAssetView value={balance.get(asset)} prefix="~" />
+          </StyledFootnote>
         </Flex.Item>
       </Flex>
     </Box>
