df964398c4e692c84179b4dd853a55a847f12ea4
Sergey Koshechkin <serge.koshechkin@gmail.com>
2022-07-18 20:18:42 +0300

    fix theme switch logic

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,28 +1,19 @@
 import { Switch } from '@ergolabs/ui-kit';
 import { SwitchProps } from 'antd';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 
 import { useSettings } from '../../context';
 
 const ThemeSwitch: React.FC<SwitchProps> = (): JSX.Element => {
   const [settings, setSettings] = useSettings();
   const { theme } = settings;
-  const [isDark, setIsDark] = useState(theme === 'dark');
+  const isDark = theme === 'dark';
 
   const handleChangeTheme = useCallback(() => {
-    if (isDark) {
-      setIsDark(false);
-      setSettings({
-        ...settings,
-        theme: 'light',
-      });
-    } else {
-      setIsDark(true);
-      setSettings({
-        ...settings,
-        theme: 'dark',
-      });
-    }
+    setSettings({
+      ...settings,
+      theme: isDark ? 'light' : 'dark',
+    });
   }, [isDark, settings, setSettings]);
 
   return (
