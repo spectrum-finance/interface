0f03f80e02e182cfe368bf7863a54645037c95f6
Sergei Koshechkin <serge.koshechkin@gmail.com>
2022-09-09 19:10:40 +0300

    add farm stake

--- src/pages/Farm/FarmStakeModal/FarmStakeModal.tsx
+++ src/pages/Farm/FarmStakeModal/FarmStakeModal.tsx
@@ -1,1 +1,126 @@
-export const FarmStakeModal = {};
+import {
+  ArrowLeftOutlined,
+  Box,
+  Divider,
+  Flex,
+  Form,
+  Modal,
+  Slider,
+  Typography,
+  useForm,
+} from '@ergolabs/ui-kit';
+import { t, Trans } from '@lingui/macro';
+import React from 'react';
+import styled from 'styled-components';
+
+import { AmmPool } from '../../../common/models/AmmPool';
+import { AssetInfo } from '../../../common/models/AssetInfo';
+import { AssetIcon } from '../../../components/AssetIcon/AssetIcon';
+import { AssetIconPair } from '../../../components/AssetIconPair/AssetIconPair';
+import { DataTag } from '../../../components/common/DataTag/DataTag';
+import { FormPairSection } from '../../../components/common/FormView/FormPairSection/FormPairSection';
+import { FormSlider } from '../../../components/common/FormView/FormSlider/FormSlider';
+import { OperationForm } from '../../../components/OperationForm/OperationForm';
+import { PageSection } from '../../../components/Page/PageSection/PageSection';
+import { Truncate } from '../../../components/Truncate/Truncate';
+import { FarmHeaderAssets } from '../FarmGridView/FarmCardView/FarmCardView';
+
+interface FarmStakeModalProps {
+  pool: AmmPool;
+  onClose: (request?: any) => void;
+}
+
+interface FarmStakeHeaderProps {
+  className?: string;
+  assetX: AssetInfo;
+  assetY: AssetInfo;
+}
+
+const _FarmStakeHeader: React.FC<FarmStakeHeaderProps> = ({
+  className,
+  assetX,
+  assetY,
+}) => {
+  return (
+    <Flex className={className} col gap={8}>
+      Stake {assetX.ticker}/{assetY.ticker} liquidity
+      <Flex justify="space-between">
+        <Flex col align="flex-start">
+          <Typography.Text>
+            <Trans>Total Staked</Trans>
+          </Typography.Text>
+          <DataTag content="$---" />
+        </Flex>
+        <Flex col align="flex-end">
+          <Typography.Text>
+            <Trans>APR</Trans>
+          </Typography.Text>
+          <DataTag content="30%" />
+        </Flex>
+        <FarmHeaderAssets>
+          <AssetIconPair assetX={assetX} assetY={assetY} size="extraLarge" />
+        </FarmHeaderAssets>
+      </Flex>
+    </Flex>
+  );
+};
+
+export const FarmStakeHeader = styled(_FarmStakeHeader)`
+  position: relative;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  background: linear-gradient(180deg, #764ca3 0%, #677de7 100%) !important;
+  padding: 16px;
+  margin: -16px -16px 32px;
+  height: 164px;
+`;
+
+export const Caption = styled(Typography.Body)`
+  font-size: 16px !important;
+  line-height: 24px !important;
+`;
+
+const marks = {
+  0: '0%',
+  25: '25%',
+  50: '50%',
+  75: '75%',
+  100: 'Max',
+};
+
+export const FarmStakeModal: React.FC<FarmStakeModalProps> = ({ pool }) => {
+  const form = useForm<any>({
+    percent: 50,
+  });
+  return (
+    <>
+      <Modal.Title>
+        <FarmStakeHeader assetX={pool.x.asset} assetY={pool.y.asset} />
+      </Modal.Title>
+      <Modal.Content maxWidth={480} width="100%">
+        <OperationForm
+          analytics={{ location: 'create-farm' }}
+          form={form}
+          onSubmit={() => console.log('confirm')}
+          actionCaption={t`Withdraw`}
+        >
+          use Box
+          <PageSection title={t`Amount`} noPadding>
+            <Flex gap={4} col>
+              <Form.Item name="percent">
+                {({ value, onChange }) => (
+                  <FormSlider value={value} onChange={onChange} />
+                )}
+              </Form.Item>
+              {/*<FormPairSection*/}
+              {/*  title={t`Assets to remove`}*/}
+              {/*  xAmount={pool.x}*/}
+              {/*  yAmount={pool.y}*/}
+              {/*/>*/}
+            </Flex>
+          </PageSection>
+        </OperationForm>
+      </Modal.Content>
+    </>
+  );
+};
--- src/pages/Farm/FarmTableView/FarmTableView.tsx
+++ src/pages/Farm/FarmTableView/FarmTableView.tsx
@@ -1,18 +1,20 @@
-import { Button, Flex, Progress, Typography } from '@ergolabs/ui-kit';
+import { Button, Flex } from '@ergolabs/ui-kit';
 import { Trans } from '@lingui/macro';
 import React, { FC } from 'react';
 
 import { AmmPool } from '../../../common/models/AmmPool';
 import { Position } from '../../../common/models/Position';
 import { DataTag } from '../../../components/common/DataTag/DataTag';
+import {
+  openConfirmationModal,
+  Operation,
+} from '../../../components/ConfirmationModal/ConfirmationModal';
 import { InfoTooltip } from '../../../components/InfoTooltip/InfoTooltip';
 import { ExpandComponentProps } from '../../../components/TableView/common/Expand';
 import { TableView } from '../../../components/TableView/TableView';
-import { AprColumn } from '../../Liquidity/common/columns/PoolsOrPositionsColumns/columns/AprColumn/AprColumn';
-import { PairColumn } from '../../Liquidity/common/columns/PoolsOrPositionsColumns/columns/PairColumn/PairColumn';
-import { TvlOrVolume24Column } from '../../Liquidity/common/columns/PoolsOrPositionsColumns/columns/TvlOrVolume24Column/TvlOrVolume24Column';
 import { LiquiditySearchState } from '../../Liquidity/common/tableViewStates/LiquiditySearchState/LiquiditySearchState';
 import { FarmPairColumn } from '../FarmPairColumn/FarmPairColumn';
+import { FarmStakeModal } from '../FarmStakeModal/FarmStakeModal';
 import { LineProgress } from '../LineProgress/LineProgress';
 import { FarmTableLoadingState } from './FarmTableLoadingState';
 
@@ -30,90 +32,117 @@ export const FarmTableView: FC<FarmTableViewProps<any>> = ({
   // poolMapper,
   loading,
   className,
-}) => (
-  <TableView
-    items={items}
-    itemKey="id"
-    itemHeight={80}
-    maxHeight={500}
-    gap={2}
-    tableHeaderPadding={[0, 6]}
-    tableItemViewPadding={[0, 4]}
-    expand={{ height: 96, accordion: true, component: expandComponent }}
-  >
-    <TableView.Column width={311} headerWidth={303} title={<Trans>Pair</Trans>}>
-      {(ammPool) => <FarmPairColumn ammPool={ammPool} status="Scheduled" />}
-    </TableView.Column>
-    <TableView.Column width={177} title={<Trans>Total Staked</Trans>}>
-      {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).tvl} />}*/}
-      {(ammPool) => (
-        <Flex>
-          <DataTag
-            content={
-              <Flex gap={1} align="center">
-                $340k
-                <InfoTooltip
-                  width={194}
-                  size="small"
-                  placement="top"
-                  content={
-                    <div>
-                      <div>ERG: 314,756.66</div>
-                      <div>SFP: 314,756.66</div>
-                    </div>
-                  }
-                />
-              </Flex>
-            }
-          />
-        </Flex>
-      )}
-    </TableView.Column>
-    <TableView.Column width={200} title={<Trans>Your Stake</Trans>}>
-      {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).volume} />}*/}
-      {(ammPool) => (
-        <Flex>
-          <DataTag content={<div>$---</div>} />
-        </Flex>
-      )}
-    </TableView.Column>
-    <TableView.Column
-      width={158}
-      title={
-        <InfoTooltip
-          width={194}
-          placement="top"
-          content={
-            <Trans>
-              345 Neta out of 1000 Neta have already been distributed
-            </Trans>
-          }
-        >
-          <Trans>Distributed</Trans>
-        </InfoTooltip>
-      }
+}) => {
+  const openStakeModal = (pool: AmmPool) => {
+    openConfirmationModal(
+      (next) => {
+        return <FarmStakeModal pool={pool} onClose={() => {}} />;
+      },
+      Operation.CREATE_FARM,
+      {},
+    );
+    // panalytics.submitDeposit(value);
+  };
+  return (
+    <TableView
+      items={items}
+      itemKey="id"
+      itemHeight={80}
+      maxHeight={500}
+      gap={2}
+      tableHeaderPadding={[0, 6]}
+      tableItemViewPadding={[0, 4]}
+      expand={{ height: 96, accordion: true, component: expandComponent }}
     >
-      {/*{(ammPool: AmmPool) => <AprColumn ammPool={poolMapper(ammPool)} />}*/}
-      {/*{(ammPool) => <Progress percent={90} />}*/}
-      {(ammPool) => <LineProgress percent={60} height={24} width="130px" />}
-    </TableView.Column>
-    <TableView.Column width={200} title={<Trans>APY</Trans>}>
-      {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).volume} />}*/}
-      {(ammPool) => (
-        <Flex>
-          <DataTag content={<div>30%</div>} />
-        </Flex>
-      )}
-    </TableView.Column>
-    <TableView.Column width={200} title={<Trans>Actions</Trans>}>
-      {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).volume} />}*/}
-      {(ammPool) => <Button type="primary">Stake</Button>}
-    </TableView.Column>
-    <TableView.State name="loading" condition={loading}>
-      <FarmTableLoadingState />
-    </TableView.State>
-    <TableView.State name="search" condition={!items.length}>
-      <LiquiditySearchState />
-    </TableView.State>
-  </TableView>
-);
+      <TableView.Column
+        width={311}
+        headerWidth={303}
+        title={<Trans>Pair</Trans>}
+      >
+        {(ammPool) => <FarmPairColumn ammPool={ammPool} status="Scheduled" />}
+      </TableView.Column>
+      <TableView.Column width={177} title={<Trans>Total Staked</Trans>}>
+        {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).tvl} />}*/}
+        {(ammPool) => (
+          <Flex>
+            <DataTag
+              content={
+                <Flex gap={1} align="center">
+                  $340k
+                  <InfoTooltip
+                    width={194}
+                    size="small"
+                    placement="top"
+                    content={
+                      <div>
+                        <div>ERG: 314,756.66</div>
+                        <div>SFP: 314,756.66</div>
+                      </div>
+                    }
+                  />
+                </Flex>
+              }
+            />
+          </Flex>
+        )}
+      </TableView.Column>
+      <TableView.Column width={200} title={<Trans>Your Stake</Trans>}>
+        {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).volume} />}*/}
+        {(ammPool) => (
+          <Flex>
+            <DataTag content={<div>$---</div>} />
+          </Flex>
+        )}
+      </TableView.Column>
+      <TableView.Column
+        width={158}
+        title={
+          <InfoTooltip
+            width={194}
+            placement="top"
+            content={
+              <Trans>
+                345 Neta out of 1000 Neta have already been distributed
+              </Trans>
+            }
+          >
+            <Trans>Distributed</Trans>
+          </InfoTooltip>
+        }
+      >
+        {/*{(ammPool: AmmPool) => <AprColumn ammPool={poolMapper(ammPool)} />}*/}
+        {/*{(ammPool) => <Progress percent={90} />}*/}
+        {(ammPool) => <LineProgress percent={60} height={24} width="130px" />}
+      </TableView.Column>
+      <TableView.Column width={200} title={<Trans>APY</Trans>}>
+        {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).volume} />}*/}
+        {(ammPool) => (
+          <Flex>
+            <DataTag content={<div>30%</div>} />
+          </Flex>
+        )}
+      </TableView.Column>
+      <TableView.Column width={200} title={<Trans>Actions</Trans>}>
+        {/*{(ammPool) => <TvlOrVolume24Column usd={poolMapper(ammPool).volume} />}*/}
+        {(ammPool) => (
+          <Button
+            type="primary"
+            onClick={(event) => {
+              console.log(ammPool);
+              openStakeModal(ammPool);
+              event.stopPropagation();
+            }}
+          >
+            Stake
+          </Button>
+        )}
+      </TableView.Column>
+      <TableView.State name="loading" condition={loading}>
+        <FarmTableLoadingState />
+      </TableView.State>
+      <TableView.State name="search" condition={!items.length}>
+        <LiquiditySearchState />
+      </TableView.State>
+    </TableView>
+  );
+};
