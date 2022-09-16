a0cc0f02d6b7939101407ee50eba075cc776d7bb
ridel1e <ridellee@gmail.com>
2022-09-16 19:07:03 +0300

    fix ergo settings behavior

--- src/network/ergo/settings/nitro.ts
+++ src/network/ergo/settings/nitro.ts
@@ -2,7 +2,7 @@ import { map, Observable, publishReplay, refCount } from 'rxjs';
 
 import { useObservable } from '../../../common/hooks/useObservable';
 import { Percent } from '../../../common/types';
-import { settings, settings$ } from './settings';
+import { getSettings, settings$ } from './settings';
 
 export const nitro$: Observable<Percent> = settings$.pipe(
   map((settings) => settings.nitro),
@@ -11,7 +11,7 @@ export const nitro$: Observable<Percent> = settings$.pipe(
 );
 
 export const useNitro = (): Percent => {
-  const [nitro] = useObservable(nitro$, [], settings.nitro);
+  const [nitro] = useObservable(nitro$, [], getSettings().nitro);
 
   return nitro;
 };
