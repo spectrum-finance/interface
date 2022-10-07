b68e6963adedb37821a1aa86e29f0495fcfbb593
ridel1e <ridellee@gmail.com>
2022-10-08 01:00:10 +0300

    add ergopay enable flow

--- src/network/ergo/api/wallet/readonly/readonly.tsx
+++ src/network/ergo/api/wallet/readonly/readonly.tsx
@@ -7,26 +7,36 @@ import { explorer } from '../../../../../services/explorer';
 import { ErgoWalletContract } from '../common/ErgoWalletContract';
 import { ReactComponent as ReadonlyLogo } from './readonly-logo-icon.svg';
 
-export const lsKey = 'readonly-ergopay-wallet';
+export const READ_ONLY_ADDRESS_KEY = 'readonly-ergopay-wallet';
+
+export const setReadonlyAddress = (address: string): void =>
+  localStorageManager.set<string>(READ_ONLY_ADDRESS_KEY, address);
+
+export const hasReadonlyAddress = (): boolean =>
+  !!localStorageManager.get(READ_ONLY_ADDRESS_KEY);
 
 export const ReadonlyWallet: ErgoWalletContract = {
   name: 'Read-only Wallet',
+  hidden: true,
   icon: <ReadonlyLogo />,
   previewIcon: <ReadonlyLogo width={21} height={21} />,
   connectWallet: () => of(true),
   getUtxos: () =>
     from(
       explorer.searchUnspentBoxesByAddresses([
-        localStorageManager.get<string>(lsKey)!,
+        localStorageManager.get<string>(READ_ONLY_ADDRESS_KEY)!,
       ]),
     ).pipe(
       map((bs: any) => bs?.map((b: any) => ergoBoxFromProxy(b))),
       map((data) => data ?? []),
     ),
-  getUsedAddresses: () => of([localStorageManager.get<string>(lsKey)!]),
+  getUsedAddresses: () =>
+    of([localStorageManager.get<string>(READ_ONLY_ADDRESS_KEY)!]),
   getUnusedAddresses: () => of([]),
-  getChangeAddress: () => of(localStorageManager.get<string>(lsKey)!),
-  getAddresses: () => of([localStorageManager.get<string>(lsKey)!]),
+  getChangeAddress: () =>
+    of(localStorageManager.get<string>(READ_ONLY_ADDRESS_KEY)!),
+  getAddresses: () =>
+    of([localStorageManager.get<string>(READ_ONLY_ADDRESS_KEY)!]),
   sign: () => ({} as any),
   signInput: () => ({} as any),
   submitTx: () => ({} as any),
