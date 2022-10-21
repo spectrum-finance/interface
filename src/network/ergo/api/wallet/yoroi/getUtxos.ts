9cb637126bce1fe84553a9277014dda6d88713ce
koutelier <koutelier@protonmail.com>
2022-10-21 19:01:26 +0300

    removing unnecessary yoroi files , removing recommended tag on wallets modal

--- src/network/ergo/api/wallet/yoroi/getUtxos.ts
+++
@@ -1,8 +0,0 @@
-import { ErgoBox, ergoBoxFromProxy } from '@ergolabs/ergo-sdk';
-import { from, map, Observable } from 'rxjs';
-
-export const getUtxos = (): Observable<ErgoBox[]> =>
-  from(ergo.get_utxos()).pipe(
-    map((bs) => bs?.map((b) => ergoBoxFromProxy(b))),
-    map((data) => data ?? []),
-  );
