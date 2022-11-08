88cfd2870a26287dabe04356e968f1bec21ecedf
ridel1e <ridellee@gmail.com>
2022-11-08 11:46:51 +0300

    fix SwapConfirmationInfo.tsx

--- src/network/ergo/widgets/common/SwapConfirmationInfo/SwapConfirmationInfo.tsx
+++ src/network/ergo/widgets/common/SwapConfirmationInfo/SwapConfirmationInfo.tsx
@@ -93,11 +93,9 @@ export const SwapConfirmationInfo: FC<SwapConfirmationInfoProps> = ({
               minOutput &&
               maxOutput && (
                 <Typography.Body size="large" strong>
-                  {`${minOutput?.toString()} - ${maxOutput?.toString()} ${
-                    value.toAsset?.ticker
-                  }`}
+                  {`${minOutput?.toString()} - ${maxOutput?.toString()}`}{' '}
                   <Truncate>
-                    {maxOutput?.asset.ticker || maxOutput?.asset.ticker}
+                    {maxOutput?.asset.ticker || maxOutput?.asset.name}
                   </Truncate>
                 </Typography.Body>
               )
