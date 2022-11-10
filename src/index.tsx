8656c17d1fa2a677d49f1a143e320305c9543962
deadit <stdeadit@gmail.com>
2022-11-10 16:40:45 +0300

    fix bug with ios under 14 version

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,39 @@ import '@ergolabs/ui-kit/dist/styles/fonts/fonts.less';
 import './assets/styles/styles.less';
 
 import React from 'react';
+import { isIOS, isMobile, mobileModel, osVersion } from 'react-device-detect';
 import ReactDOM from 'react-dom';
 
 import { ApplicationInitializer } from './App';
 import { reportWebVitals } from './reportWebVitals';
 
-// TODO: fix toggle-group behavior after switch to react v18 root api.
-ReactDOM.render(
-  <React.StrictMode>
-    <ApplicationInitializer />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const init = () => {
+  if (
+    isIOS &&
+    osVersion <= '14' &&
+    isMobile &&
+    navigator.platform.indexOf('Mac') === -1
+  ) {
+    ReactDOM.render(
+      <h1>
+        {osVersion}, {isMobile.toString()}
+      </h1>,
+      document.getElementById('root'),
+    );
+    return;
+  }
 
+  // ReactDOM.render(<h1>text</h1>, document.getElementById('root'));
+  // TODO: fix toggle-group behavior after switch to react v18 root api.
+  ReactDOM.render(
+    <React.StrictMode>
+      <ApplicationInitializer />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+};
+
+init();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
