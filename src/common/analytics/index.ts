800e07fe7c647e0de3dc9fef848c39e40dee6a14
deadit <stdeadit@gmail.com>
2022-12-26 15:11:20 +0300

    fix analytic

--- src/common/analytics/index.ts
+++ src/common/analytics/index.ts
@@ -3,10 +3,11 @@ import { AmplitudeAnalyticSystem } from './system/AmplitudeAnalyticSystem';
 import { PostHogAnalyticSystem } from './system/PostHogAnalyticSystem';
 
 export const panalytics = new ProductAnalytics(
-  process.env.REACT_APP_POSTHOG_API_KEY && process.env.production
+  process.env.REACT_APP_POSTHOG_API_KEY && process.env.NODE_ENV === 'production'
     ? new PostHogAnalyticSystem()
     : undefined,
-  process.env.REACT_APP_AMPLITUDE_API_KEY && process.env.production
+  process.env.REACT_APP_AMPLITUDE_API_KEY &&
+  process.env.NODE_ENV === 'production'
     ? new AmplitudeAnalyticSystem()
     : undefined,
 );
