9e4ce493794543df169e1002ca498a9b13b73d37
ridel1e <ridellee@gmail.com>
2023-03-10 16:55:44 +0300

    update pagination buttons

--- src/components/OperationHistoryModal/OperationHistoryV2/OperationPagination/OperationPagination.tsx
+++ src/components/OperationHistoryModal/OperationHistoryV2/OperationPagination/OperationPagination.tsx
@@ -1,4 +1,12 @@
-import { Button, ButtonProps, Flex } from '@ergolabs/ui-kit';
+import {
+  Button,
+  ButtonProps,
+  DoubleLeftOutlined,
+  DoubleRightOutlined,
+  Flex,
+  LeftOutlined,
+  RightOutlined,
+} from '@ergolabs/ui-kit';
 import React, { FC } from 'react';
 import styled, { css } from 'styled-components';
 
@@ -7,6 +15,35 @@ const _PaginationButton: FC<ButtonProps & { active?: boolean }> = ({
 }) => <Button {...rest} />;
 
 const PaginationButton = styled(_PaginationButton)`
+  &.ant-btn:disabled,
+  &.ant-btn-lg:disabled,
+  &.ant-btn.ant-btn-sm:disabled,
+  &.ant-btn-icon-only:disabled,
+  &.ant-btn.ant-btn-text:disabled,
+  &.ant-btn:disabled:active,
+  &.ant-btn-lg:disabled:active,
+  &.ant-btn.ant-btn-sm:disabled:active,
+  &.ant-btn-icon-only:disabled:active,
+  &.ant-btn.ant-btn-text:disabled:active,
+  &.ant-btn:disabled:focus,
+  &.ant-btn-lg:disabled:focus,
+  &.ant-btn.ant-btn-sm:disabled:focus,
+  &.ant-btn-icon-only:disabled:focus,
+  &.ant-btn.ant-btn-text:disabled:focus,
+  &.ant-btn:disabled:hover,
+  &.ant-btn-lg:disabled:hover,
+  &.ant-btn.ant-btn-sm:disabled:hover,
+  &.ant-btn-icon-only:disabled:hover,
+  &.ant-btn.ant-btn-text:disabled:hover,
+  &.ant-btn:disabled .ant-btn-loading,
+  &.ant-btn-lg:disabled .ant-btn-loading,
+  &.ant-btn.ant-btn-sm:disabled .ant-btn-loading,
+  &.ant-btn-icon-only:disabled .ant-btn-loading,
+  &.ant-btn.ant-btn-text:disabled .ant-btn-loading {
+    background: transparent !important;
+    border-color: transparent !important;
+  }
+
   ${(props) =>
     props.active
       ? css`
@@ -58,14 +95,29 @@ export const OperationPagination: FC<OperationPaginationProps> = ({
       activePage + 2,
     ];
   }
+  const isActivePageFirst = activePage === 1;
+  const isActivePageLast = activePage === pageCount;
 
   return (
     <Flex align="center" justify="center">
+      <Flex.Item marginRight={1}>
+        <PaginationButton
+          onClick={() => handlePageSelect(1)}
+          width={24}
+          disabled={isActivePageFirst}
+          icon={<DoubleLeftOutlined />}
+        />
+      </Flex.Item>
+      <Flex.Item marginRight={1}>
+        <PaginationButton
+          onClick={() => handlePageSelect(activePage - 1)}
+          width={24}
+          disabled={isActivePageFirst}
+          icon={<LeftOutlined />}
+        />
+      </Flex.Item>
       {displayedPages.map((page, i) => (
-        <Flex.Item
-          key={page}
-          marginRight={displayedPages.length - 1 === i ? 0 : 1}
-        >
+        <Flex.Item key={page} marginRight={1}>
           <PaginationButton
             width={34}
             onClick={() => handlePageSelect(page)}
@@ -78,6 +130,22 @@ export const OperationPagination: FC<OperationPaginationProps> = ({
           </PaginationButton>
         </Flex.Item>
       ))}
+      <Flex.Item marginRight={1}>
+        <PaginationButton
+          onClick={() => handlePageSelect(activePage + 1)}
+          width={24}
+          disabled={isActivePageLast}
+          icon={<RightOutlined />}
+        />
+      </Flex.Item>
+      <Flex.Item>
+        <PaginationButton
+          onClick={() => handlePageSelect(pageCount)}
+          width={24}
+          disabled={isActivePageLast}
+          icon={<DoubleRightOutlined />}
+        />
+      </Flex.Item>
     </Flex>
   );
 };
