a116b6a747d8802e1491ba565284d4d5ce6e1489
ridel1e <ridellee@gmail.com>
2023-04-20 16:28:05 +0300

    update txFee Mappting

--- src/network/cardano/api/operations/common/ammTxFeeMapping.ts
+++ src/network/cardano/api/operations/common/ammTxFeeMapping.ts
@@ -1,10 +1,10 @@
 import { AmmTxFeeMapping } from '@ergolabs/cardano-dex-sdk';
 
 export const ammTxFeeMapping: AmmTxFeeMapping = {
-  swapOrder: 1000000n,
+  swapOrder: 2000000n,
   depositOrder: 1000000n,
   redeemOrder: 1000000n,
-  swapExecution: 1000000n,
+  swapExecution: 2000000n,
   depositExecution: 1000000n,
   redeemExecution: 1000000n,
 };
