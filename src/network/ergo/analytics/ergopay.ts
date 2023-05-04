bac0aae421b32c7173516fc015964b6814d118f9
ridel1e <ridellee@gmail.com>
2023-05-04 13:06:00 +0300

    add ergopay analytics

---
+++ src/network/ergo/analytics/ergopay.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import {
+  distinctUntilKeyChanged,
+  exhaustMap,
+  first,
+  map,
+  mapTo,
+  Observable,
+  publishReplay,
+  refCount,
+  switchMap,
+} from 'rxjs';
+
+import { applicationConfig } from '../../../applicationConfig';
+import { appTick$ } from '../../../common/streams/appTick';
+import { settings$ } from '../settings/settings';
+
+interface TxForMark {
+  readonly ergopayId: string;
+  readonly txInfo: object;
+  readonly txId: string;
+  readonly status: 'success' | 'error';
+}
+
+const ergoPayAnalytics$: Observable<TxForMark[]> = settings$.pipe(
+  switchMap((settings) => appTick$.pipe(mapTo(settings))),
+  exhaustMap(({ address }) =>
+    axios.get<TxForMark[]>(
+      `${applicationConfig.networksSettings.ergo.ergopayUrl}/unsignedTx/${address}/waiting_for_mark`,
+    ),
+  ),
+  map((res) => res.data),
+  distinctUntilKeyChanged('lenght'),
+  publishReplay(1),
+  refCount(),
+);
+
+const markTxs = (txs: TxForMark[], address: string): void => {
+  settings$
+    .pipe(first())
+    .subscribe((settings) =>
+      axios.post(
+        `${applicationConfig.networksSettings.ergo.ergopayUrl}/unsignedTx/${address}/mark`,
+        { transactions: txs.map((tx) => tx.ergopayId) },
+      ),
+    );
+};
+
+export const initializeErgoPayAnalytics = () => {
+  ergoPayAnalytics$.subscribe((txsForMark) => {
+
+  });
+};
--- src/network/ergo/initialized.ts
+++ src/network/ergo/initialized.ts
@@ -1,6 +1,10 @@
 import { RustModule } from '@ergolabs/ergo-sdk';
 import { BehaviorSubject } from 'rxjs';
 
+import {
+  ergoPayAnalytics$,
+  initializeErgoPayAnalytics,
+} from './analytics/ergopay';
 import { initializeSettings } from './settings/settings';
 
 export const initialized$ = new BehaviorSubject(false);
@@ -8,6 +12,7 @@ export const initialized$ = new BehaviorSubject(false);
 export const initialize = (): void => {
   RustModule.load().then(() => {
     initializeSettings();
+    initializeErgoPayAnalytics();
     initialized$.next(true);
   });
 };
