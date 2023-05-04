81e2207d1bd1cb54350dfcf968d17645ac234221
ridel1e <ridellee@gmail.com>
2023-05-04 13:10:38 +0300

    fix lint errors #2

--- src/network/ergo/initialized.ts
+++ src/network/ergo/initialized.ts
@@ -1,10 +1,7 @@
 import { RustModule } from '@ergolabs/ergo-sdk';
 import { BehaviorSubject } from 'rxjs';
 
-import {
-  ergoPayAnalytics$,
-  initializeErgoPayAnalytics,
-} from './analytics/ergopay';
+import { initializeErgoPayAnalytics } from './analytics/ergopay';
 import { initializeSettings } from './settings/settings';
 
 export const initialized$ = new BehaviorSubject(false);
