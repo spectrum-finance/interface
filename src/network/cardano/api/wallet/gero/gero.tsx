b44f7b780eef13821dc836a77972da3fc2a669c7
yashablack <iamyashablack@gmail.com>
2023-05-19 14:55:54 +0300

    fix: change gero wallet name and link

--- src/network/cardano/api/wallet/gero/gero.tsx
+++ src/network/cardano/api/wallet/gero/gero.tsx
@@ -5,10 +5,12 @@ import GeroWalletLogo from './gerowallet-icon.svg';
 export const Gero: CardanoWalletContract = makeCardanoWallet({
   variableName: 'gerowallet',
   extensionLink:
-    'https://chrome.google.com/webstore/detail/gerowallet-preview/iifeegfcfhlhhnilhfoeihllenamcfgc',
+    'https://chrome.google.com/webstore/detail/gerowallet/bgpipimickeadkjlklgciifhnalhdjhe/overview',
   walletSupportedFeatures: { createPool: false },
-  name: 'GeroWallet Preview',
+  name: 'Gero Wallet',
   testnetSwitchGuideUrl: '',
-  icon: <img src={GeroWalletLogo} height={32} width={32} />,
-  previewIcon: <img src={GeroWalletLogo} width={21} height={21} />,
+  icon: <img alt="Gero Wallet" src={GeroWalletLogo} height={32} width={32} />,
+  previewIcon: (
+    <img alt="Gero Wallet" src={GeroWalletLogo} width={21} height={21} />
+  ),
 });
