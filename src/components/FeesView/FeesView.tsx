e91b637a96b1518a8c8e01ff51bc8c27e58b9a7c
yashablack <iamyashablack@gmail.com>
2023-05-20 16:17:10 +0300

    fix: refactor FeesView.tsx component

--- src/components/FeesView/FeesView.tsx
+++ src/components/FeesView/FeesView.tsx
@@ -1,123 +1,126 @@
 import { Flex, Typography } from '@ergolabs/ui-kit';
 import { Trans } from '@lingui/macro';
-import { FC, ReactNode } from 'react';
+// import { isArray } from 'lodash';
+import { FC } from 'react';
 
 import { Currency } from '../../common/models/Currency';
-import { useSelectedNetwork } from '../../gateway/common/network';
+import RefundableDepositTooltipContent from '../../network/cardano/components/RefundableDepositTooltipContent/RefundableDepositTooltipContent.tsx';
+// import { useSelectedNetwork } from '../../gateway/common/network';
 import { AssetIcon } from '../AssetIcon/AssetIcon';
 import { BoxInfoItem } from '../BoxInfoItem/BoxInfoItem';
 import { ConvenientAssetView } from '../ConvenientAssetView/ConvenientAssetView';
 import { InfoTooltip } from '../InfoTooltip/InfoTooltip';
-import { IsErgo } from '../IsErgo/IsErgo';
+import { Truncate } from '../Truncate/Truncate.tsx';
 
 export interface FeesViewItem {
-  caption: ReactNode | ReactNode[] | string;
-  currency: Currency | [Currency, Currency];
-}
-
-export interface TotalFees {
-  minFeesForTotal: Currency[];
-  maxFeesForTotal: Currency[];
+  caption: string;
+  currency: Currency;
 }
 
 export interface FeesViewProps {
   readonly fees: FeesViewItem[];
-  readonly totalFees: Currency[] | TotalFees;
+  readonly executionFee: [Currency, Currency];
+  readonly refundableDeposit?: Currency;
 }
 
-export const FeesView: FC<FeesViewProps> = ({ fees, totalFees }) => {
-  const [selectedNetwork] = useSelectedNetwork();
-  const sumTotalFees = (fees: Currency[]): Currency => {
-    return fees.reduce(
-      (sum, f) => sum.plus(f),
-      new Currency(0n, fees[0].asset),
-    );
-  };
+export const FeesView: FC<FeesViewProps> = ({
+  fees,
+  executionFee,
+  refundableDeposit,
+}) => {
+  // const [selectedNetwork] = useSelectedNetwork();
+  const feeSum: Currency = fees.reduce((acc, fee) => {
+    return acc.plus(fee.currency);
+  }, new Currency(0n, fees[0].currency.asset));
 
   return (
-    <BoxInfoItem
-      title={
-        <>
-          <InfoTooltip
-            placement="right"
-            content={
-              <Flex col>
-                {fees.map((f, i) => (
-                  <Flex.Item
-                    display="flex"
-                    align="center"
-                    key={i}
-                    marginBottom={i === fees.length - 1 ? 0 : 1}
-                  >
-                    <Flex.Item marginRight={1}>{f.caption}:</Flex.Item>
+    <>
+      {refundableDeposit && (
+        <Flex.Item marginBottom={2}>
+          <BoxInfoItem
+            title={
+              <>
+                <InfoTooltip
+                  content={<RefundableDepositTooltipContent />}
+                  width={300}
+                >
+                  <Typography.Body size="large">
+                    <Trans>Refundable deposit</Trans>
+                  </Typography.Body>
+                </InfoTooltip>
+                <Typography.Body size="large">:</Typography.Body>
+              </>
+            }
+            value={
+              <Typography.Body size="large" strong>
+                <>
+                  {refundableDeposit.toString()}{' '}
+                  <Truncate>{refundableDeposit.asset.name}</Truncate>
+                </>
+              </Typography.Body>
+            }
+          />
+        </Flex.Item>
+      )}
+
+      <BoxInfoItem
+        title={
+          <>
+            <InfoTooltip
+              placement="right"
+              content={
+                <Flex col>
+                  <Flex.Item display="flex" align="center" marginBottom={1}>
+                    <Flex.Item marginRight={1}>Execution Fee:</Flex.Item>
                     <Flex.Item align="center" display="flex">
                       <Flex.Item marginRight={1}>
                         <AssetIcon
                           size="extraSmall"
-                          asset={
-                            f.currency instanceof Array
-                              ? f.currency[0].asset
-                              : f.currency.asset
-                          }
+                          asset={executionFee[0].asset}
                         />
                       </Flex.Item>
-                      {f.currency instanceof Array
-                        ? `${f.currency[0].toString()} - ${f.currency[1].toString()} ${
-                            f.currency[0].asset.ticker
-                          }`
-                        : f.currency.toCurrencyString()}{' '}
-                      <IsErgo>
-                        (
-                        {f.currency instanceof Array ? (
-                          <>
-                            <ConvenientAssetView value={f.currency[0]} /> -{' '}
-                            <ConvenientAssetView value={f.currency[1]} />
-                          </>
-                        ) : (
-                          <ConvenientAssetView value={f.currency} />
-                        )}
-                        )
-                      </IsErgo>
+                      {`${executionFee[0].toString()} - ${executionFee[1].toString()} ${
+                        executionFee[0].asset.ticker
+                      }`}
                     </Flex.Item>
                   </Flex.Item>
-                ))}
-              </Flex>
-            }
-          >
-            <Typography.Body size="large">
-              <Trans>Total Fees</Trans>
-            </Typography.Body>
-          </InfoTooltip>
-          <Typography.Body size="large">:</Typography.Body>
-        </>
-      }
-      value={
-        <Typography.Body size="large" strong>
-          {selectedNetwork.name === 'cardano' && (
-            <>
-              {totalFees instanceof Array
-                ? sumTotalFees(totalFees).toCurrencyString()
-                : `${sumTotalFees(
-                    totalFees.minFeesForTotal,
-                  ).toString()} - ${sumTotalFees(
-                    totalFees.maxFeesForTotal,
-                  ).toString()} ${totalFees.minFeesForTotal[0].asset.ticker}`}
-            </>
-          )}
-          {selectedNetwork.name === 'ergo' && (
-            <>
-              {totalFees instanceof Array ? (
-                <ConvenientAssetView value={totalFees} />
-              ) : (
-                <>
-                  <ConvenientAssetView value={totalFees.minFeesForTotal} /> -{' '}
-                  <ConvenientAssetView value={totalFees.maxFeesForTotal} />
-                </>
-              )}
-            </>
-          )}
-        </Typography.Body>
-      }
-    />
+                  {fees.map((f, i) => (
+                    <Flex.Item
+                      display="flex"
+                      align="center"
+                      key={i}
+                      marginBottom={i === fees.length - 1 ? 0 : 1}
+                    >
+                      <Flex.Item marginRight={1}>{f.caption}:</Flex.Item>
+                      <Flex.Item align="center" display="flex">
+                        <Flex.Item marginRight={1}>
+                          <AssetIcon
+                            size="extraSmall"
+                            asset={f.currency.asset}
+                          />
+                        </Flex.Item>
+                        {`${f.currency.toString()} `}{' '}
+                        <Truncate>{f.currency.asset.ticker}</Truncate>
+                      </Flex.Item>
+                    </Flex.Item>
+                  ))}
+                </Flex>
+              }
+            >
+              <Typography.Body size="large">
+                <Trans>Total Fees</Trans>
+              </Typography.Body>
+            </InfoTooltip>
+            <Typography.Body size="large">:</Typography.Body>
+          </>
+        }
+        value={
+          <Typography.Body size="large" strong>
+            <ConvenientAssetView value={[executionFee[0], feeSum]} /> -{' '}
+            <ConvenientAssetView value={[executionFee[1], feeSum]} />
+          </Typography.Body>
+        }
+      />
+    </>
   );
 };
