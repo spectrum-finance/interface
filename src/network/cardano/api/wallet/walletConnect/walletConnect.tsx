47b09d982dc1748a114814cf148d3e5ff7855c51
yashablack <iamyashablack@gmail.com>
2023-06-09 17:03:58 +0300

    fix: revert wallet connect consts.ts

--- src/network/cardano/api/wallet/walletConnect/walletConnect.tsx
+++ src/network/cardano/api/wallet/walletConnect/walletConnect.tsx
@@ -7,10 +7,11 @@ import { init as initWalletConnect } from '@dcspark/adalib';
 
 import { CardanoWalletContract } from '../common/CardanoWalletContract.ts';
 import { makeCardanoWallet } from '../common/makeCardanoWallet.tsx';
+import { CONNECTOR_NAME_WALLET_CONNECT } from '../consts.ts';
 import WcLogo from './wc-logo.png';
 
 const PROJECT_ID = 'add5d2d2d1b89b17dd9871986a5fe797';
-const CONNECTOR_NAME_WALLET_CONNECT = 'walletconnect';
+
 initWalletConnect(
   () => ({
     connectorName: CONNECTOR_NAME_WALLET_CONNECT,
