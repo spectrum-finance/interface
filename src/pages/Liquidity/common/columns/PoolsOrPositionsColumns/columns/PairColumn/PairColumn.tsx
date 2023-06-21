770a33ff352f0e4d9fcddabea278df9dfd3d4adf
yashablack <iamyashablack@gmail.com>
2023-06-21 21:58:46 +0300

    feat: add lbsp tag

--- src/pages/Liquidity/common/columns/PoolsOrPositionsColumns/columns/PairColumn/PairColumn.tsx
+++ src/pages/Liquidity/common/columns/PoolsOrPositionsColumns/columns/PairColumn/PairColumn.tsx
@@ -2,17 +2,23 @@ import { Flex } from '@ergolabs/ui-kit';
 import { FC, MouseEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { applicationConfig } from '../../../../../../../applicationConfig.ts';
 import { useObservable } from '../../../../../../../common/hooks/useObservable';
 import { AmmPool } from '../../../../../../../common/models/AmmPool';
 import { AssetPairTitle } from '../../../../../../../components/AssetPairTitle/AssetPairTitle';
 import { DataTag } from '../../../../../../../components/common/DataTag/DataTag';
 import { FarmsButton } from '../../../../../../../components/FarmsButton/FarmsButton';
+import { IsCardano } from '../../../../../../../components/IsCardano/IsCardano.tsx';
 import { IsErgo } from '../../../../../../../components/IsErgo/IsErgo';
+import { LbspPoolTag } from '../../../../../../../components/LbspPoolTag/LbspPoolTag.tsx';
 import { hasFarmsForPool } from '../../../../../../../network/ergo/lm/api/farms/farms';
 
 export interface PairColumnProps {
   readonly ammPool: AmmPool;
 }
+
+const isLbspPool = (poolId: string): boolean =>
+  applicationConfig.lbspLiquidityPools.includes(poolId);
 
 export const PairColumn: FC<PairColumnProps> = ({ ammPool }) => {
   const navigate = useNavigate();
@@ -35,6 +41,7 @@ export const PairColumn: FC<PairColumnProps> = ({ ammPool }) => {
       <IsErgo>
         {hasFarmForPool && <FarmsButton onClick={handleFarmsButtonClick} />}
       </IsErgo>
+      <IsCardano>{isLbspPool(ammPool.id) && <LbspPoolTag />}</IsCardano>
     </Flex>
   );
 };
