c99a5c75d27446d61ac44881d9db0fdcfde37f87
yashablack <iamyashablack@gmail.com>
2023-06-22 14:11:29 +0300

    feat: added refund notification

--- src/components/common/Layout/OperationsHistory/OperationsHistory.tsx
+++ src/components/common/Layout/OperationsHistory/OperationsHistory.tsx
@@ -6,12 +6,17 @@ import {
   Tooltip,
 } from '@ergolabs/ui-kit';
 import { t } from '@lingui/macro';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { useObservable } from '../../../../common/hooks/useObservable';
 import { pendingOperations$ } from '../../../../gateway/api/pendingOperations';
+import { getOperations } from '../../../../gateway/api/transactionsHistory.ts';
+import { selectedWalletState$ } from '../../../../gateway/api/wallets.ts';
 import { useSelectedNetwork } from '../../../../gateway/common/network';
+import { WalletState } from '../../../../network/common/Wallet.ts';
 import { mempoolRawOperations$ } from '../../../../network/ergo/api/operations/history/v2/operationsHistory';
+import { showRefundOperationNotification } from '../../../../services/notifications/RefundOperation/RefundOperation.tsx';
+import { BadgeCustom } from '../../../BadgeCustom/BadgeCustom.tsx';
 import { OperationHistoryModal } from '../../../OperationHistoryModal/OperationHistoryModal';
 
 export const OperationsHistory: FC = () => {
@@ -21,6 +26,30 @@ export const OperationsHistory: FC = () => {
       ? mempoolRawOperations$
       : (pendingOperations$ as any),
   );
+  const [walletState] = useObservable(selectedWalletState$);
+  const [operations] = useObservable(getOperations(), [walletState]);
+  const isWalletConnected = walletState === WalletState.CONNECTED;
+
+  const [hasOperationsToRefund, setHasOperationsToRefund] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    if (operations) {
+      setHasOperationsToRefund(
+        operations.some((op) => {
+          if (op.status) {
+            return op.status === 'locked';
+          }
+        }) && isWalletConnected,
+      );
+    }
+  }, [operations]);
+
+  useEffect(() => {
+    if (hasOperationsToRefund && isWalletConnected) {
+      showRefundOperationNotification();
+    }
+  }, [hasOperationsToRefund, isWalletConnected]);
 
   const openOperationsHistoryModal = () => {
     Modal.open(({ close }) => (
@@ -35,21 +64,30 @@ export const OperationsHistory: FC = () => {
 
   return (
     <Tooltip
-      title={t`Recent transactions`}
+      title={
+        !isWalletConnected
+          ? t`Connect Wallet to see your recent transactions`
+          : hasOperationsToRefund
+          ? t`You have locked transactions. Refund them now!`
+          : t`Recent transactions`
+      }
       width="100%"
       maxWidth={200}
-      placement="bottom"
+      placement="bottomLeft"
     >
-      <Button
-        size="large"
-        type="ghost"
-        icon={showLoader ? <LoadingOutlined /> : <HistoryOutlined />}
-        onClick={openOperationsHistoryModal}
-      >
-        {!!pendingOperations?.length
-          ? `${pendingOperations?.length} ` + t`Pending`
-          : ''}
-      </Button>
+      <BadgeCustom isShow={hasOperationsToRefund}>
+        <Button
+          size="large"
+          type="ghost"
+          icon={showLoader ? <LoadingOutlined /> : <HistoryOutlined />}
+          onClick={openOperationsHistoryModal}
+          disabled={!isWalletConnected}
+        >
+          {!!pendingOperations?.length
+            ? `${pendingOperations?.length} ` + t`Pending`
+            : ''}
+        </Button>
+      </BadgeCustom>
     </Tooltip>
   );
 };
