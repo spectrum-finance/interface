dae8ccd2fc3acdf9b3dae7863b2d689e248b0ae2
yashablack <iamyashablack@gmail.com>
2023-06-22 14:10:53 +0300

    feat: added notifications

---
+++ src/services/notifications/RefundOperation/RefundOperation.tsx
@@ -0,0 +1,19 @@
+import { HistoryOutlined, notification } from '@ergolabs/ui-kit';
+
+import { notificationKeys } from '../notificationKeys';
+
+export const showRefundOperationNotification = () => {
+  notification.open({
+    key: notificationKeys.REFUND,
+    description: (
+      <>
+        Some of your funds are locked in smart contracts. Don&apos;t worry! To
+        access them, please click on the Order History button (
+        <HistoryOutlined />) to initiate a refund.
+      </>
+    ),
+    message: 'Refund order',
+    duration: 0,
+    type: 'warning',
+  });
+};
