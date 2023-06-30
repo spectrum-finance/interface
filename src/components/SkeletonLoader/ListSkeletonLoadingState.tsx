278d54db97c45e6c28109d050e5c28e858b0445f
yashablack <iamyashablack@gmail.com>
2023-06-30 14:45:23 +0300

    fix: op history loader

--- src/components/SkeletonLoader/ListSkeletonLoadingState.tsx
+++ src/components/SkeletonLoader/ListSkeletonLoadingState.tsx
@@ -3,5 +3,13 @@ import { FC } from 'react';
 import { makeSkeletonLoader } from './makeSkeletonLoader.tsx';
 import { SkeletonLoader } from './SkeletonLoader.tsx';
 
-export const ListSkeletonLoadingState: FC = () =>
-  makeSkeletonLoader(<SkeletonLoader height={78} />, 9);
+interface ListSkeletonLoadingStateProps {
+  numOfElements?: number;
+}
+export const ListSkeletonLoadingState: FC<ListSkeletonLoadingStateProps> = ({
+  numOfElements,
+}) =>
+  makeSkeletonLoader(
+    <SkeletonLoader height={78} />,
+    numOfElements ? numOfElements : 9,
+  );
