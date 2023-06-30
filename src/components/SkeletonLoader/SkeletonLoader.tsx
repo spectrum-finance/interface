4976a15d724e4d2c7093f744dfa2f3a045a7bd0b
yashablack <iamyashablack@gmail.com>
2023-06-30 15:06:16 +0300

    fix: analytics loader

--- src/components/SkeletonLoader/SkeletonLoader.tsx
+++ src/components/SkeletonLoader/SkeletonLoader.tsx
@@ -2,15 +2,17 @@ import { Skeleton } from '@ergolabs/ui-kit';
 import { FC } from 'react';
 
 interface SkeletonLoaderProps {
-  height: number;
+  height?: number;
+  width?: number;
 }
 
-export const SkeletonLoader: FC<SkeletonLoaderProps> = ({ height }) => {
+export const SkeletonLoader: FC<SkeletonLoaderProps> = ({ height, width }) => {
   return (
     <Skeleton.Block
       style={{
         borderRadius: 'var(--spectrum-border-radius-l)',
         height,
+        width,
       }}
       active
     />
---
+++ src/components/common/Layout/Header/Analytics/AnalyticsSkeletonLoader/AnalyticsSkeletonLoader.tsx
@@ -0,0 +1,5 @@
+import { SkeletonLoader } from '../../../../../SkeletonLoader/SkeletonLoader.tsx';
+
+export const AnalyticsSkeletonLoader = () => {
+  return <SkeletonLoader height={22} width={10} />;
+};
