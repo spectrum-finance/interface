cb88f76198b97ad878031876d4115d9fd59bc612
yashablack <iamyashablack@gmail.com>
2023-07-01 06:38:51 +0300

    feat: vespr test log

--- src/components/common/ConnectWalletButton/utils.ts
+++ src/components/common/ConnectWalletButton/utils.ts
@@ -1,6 +1,11 @@
 export const isVesprWallet = (): boolean => {
   // @ts-ignore
   if (window.cardano.vespr && window.cardano.vespr.experimental) {
+    console.log(
+      '>> vespr test',
+      // @ts-ignore
+      window.cardano.vespr.experimental.vespr_compat(),
+    );
     // @ts-ignore
     return window.cardano.vespr.experimental.vespr_compat();
   }
