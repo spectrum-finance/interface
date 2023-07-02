b52719170aa6717fd7ce36219718f373bc2b670f
yashablack <iamyashablack@gmail.com>
2023-07-02 15:00:26 +0300

    fix: bind props

--- src/components/SensitiveContent/SensitiveContent.tsx
+++ src/components/SensitiveContent/SensitiveContent.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from 'react';
+import { FC, ReactNode } from 'react';
 
 import { useApplicationSettings } from '../../context';
 
 interface SensitiveContent {
   children: ReactNode | string;
 }
-export const SensitiveContent = ({ children }) => {
+export const SensitiveContent: FC<SensitiveContent> = ({ children }) => {
   const [{ isSensitiveHidden }] = useApplicationSettings();
   return (
     <span style={{ filter: isSensitiveHidden ? 'blur(5px)' : 'none' }}>
