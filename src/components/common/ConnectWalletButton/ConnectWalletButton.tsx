ec575527f5244bebac9054eb874b57cb3737d259
yashablack <iamyashablack@gmail.com>
2023-07-02 13:50:50 +0300

    fix: rm vespr condition

--- src/components/common/ConnectWalletButton/ConnectWalletButton.tsx
+++ src/components/common/ConnectWalletButton/ConnectWalletButton.tsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   ButtonProps,
   ConnectWalletButton as SpectrumConnectWalletButton,
   Modal,
@@ -11,9 +10,7 @@ import { FC, ReactNode } from 'react';
 
 import { useObservable } from '../../../common/hooks/useObservable';
 import { isWalletSetuped$ } from '../../../gateway/api/wallets';
-import { selectedNetwork$ } from '../../../gateway/common/network.ts';
 import { ChooseWalletModal } from './ChooseWalletModal/ChooseWalletModal';
-import { isVesprWallet } from './utils';
 
 export interface ConnectWalletButtonProps {
   readonly size?: ButtonProps['size'];
@@ -31,16 +28,11 @@ export const ConnectWalletButton: FC<ConnectWalletButtonProps> = ({
   width,
 }) => {
   const [isWalletConnected] = useObservable(isWalletSetuped$);
-  const [network] = useObservable(selectedNetwork$);
 
   const openChooseWalletModal = (): void => {
     Modal.open(({ close }) => <ChooseWalletModal close={close} />);
     fireAnalyticsEvent('Click Connect Wallet Button', trace);
   };
-
-  if (isVesprWallet() && network?.name.includes('cardano')) {
-    return <Button>Hello</Button>;
-  }
 
   return (
     <>
