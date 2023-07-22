bf9d7060a60e6dd65469e0b49af474375235fbae
ridel1e <ridellee@gmail.com>
2023-07-22 19:13:24 +0300

    try add timer for initial set

--- src/network/cardano/api/wallet/common/WalletManager.tsx
+++ src/network/cardano/api/wallet/common/WalletManager.tsx
@@ -177,11 +177,13 @@ export const createWalletManager = (
       (w) => w.id === cacheStrategy.get(),
     );
     if (walletObject) {
-      setActiveWallet(walletObject, true).catch((isConnected) => {
-        if (!isConnected) {
-          clearWallet();
-        }
-      });
+      setTimeout(() => {
+        setActiveWallet(walletObject, true).catch((isConnected) => {
+          if (!isConnected) {
+            clearWallet();
+          }
+        });
+      }, 1_000);
     }
   }
 
