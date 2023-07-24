6c52c28df24f25ed5f4707140505122d1bef375f
yashablack <iamyashablack@gmail.com>
2023-07-24 23:13:24 +0300

    fix: Added validation for 10k ADA

--- src/utils/lbsp.ts
+++ src/utils/lbsp.ts
@@ -5,3 +5,7 @@ import { applicationConfig } from '../applicationConfig.ts';
 export const isPreLbspTimeGap = () => {
   return applicationConfig.cardanoAmmSwapsOpenTime > DateTime.now();
 };
+
+export const isLbspAmmPool = (poolId: string) => {
+  return applicationConfig.lbspLiquidityPools.some((el) => el === poolId);
+};
