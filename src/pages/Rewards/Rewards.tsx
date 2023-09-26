90f0629bfd8f4275e87876d870ac298a13efd25d
Ridel1e <Ridellee@gmail.com>
2023-09-26 15:06:04 +0300

    Dev 1403 forcing eternl (#915)
    * add eternl warning
    
    * add eternl link

--- src/pages/Rewards/Rewards.tsx
+++ src/pages/Rewards/Rewards.tsx
@@ -1,4 +1,12 @@
-import { Button, Flex, Skeleton, Typography } from '@ergolabs/ui-kit';
+import {
+  Alert,
+  Button,
+  Flex,
+  InfoCircleFilled,
+  Skeleton,
+  Typography,
+} from '@ergolabs/ui-kit';
+import { Trans } from '@lingui/macro';
 import { ElementLocation, ElementName } from '@spectrumlabs/analytics';
 import { FC } from 'react';
 
@@ -8,6 +16,7 @@ import { useObservable } from '../../common/hooks/useObservable.ts';
 import { ConnectWalletButton } from '../../components/common/ConnectWalletButton/ConnectWalletButton.tsx';
 import { Page } from '../../components/Page/Page.tsx';
 import { rewards$ } from '../../network/cardano/api/rewards/rewards';
+import { Eternl } from '../../network/cardano/api/wallet/eternl/eternl';
 import { RewardsBugFixing } from './RewardsBugFixing/RewardsBugFixing';
 import { RewardsDashboard } from './RewardsDashboard/RewardsDashboard.tsx';
 
@@ -15,35 +24,89 @@ export const Rewards: FC = () => {
   const [rewardsData, loading] = useObservable(rewards$);
 
   return (
-    <Page width={500}>
-      <Flex col>
-        <Flex.Item marginBottom={4} display="flex" justify="space-between">
-          <Typography.Title level={4}>Rewards</Typography.Title>
-          {applicationConfig.isRewardsAvailable && (
-            <Button href={SPECTRUM_DISCORD_LINK} target="_blank">
-              Support
-            </Button>
-          )}
+    <Flex col align="center">
+      {applicationConfig.isRewardsAvailable && (
+        <Flex.Item maxWidth={500} width="100%" marginBottom={3}>
+          <Alert
+            type="warning"
+            description={
+              <Flex>
+                <Flex.Item marginRight={3}>
+                  <InfoCircleFilled
+                    style={{ color: 'var(--spectrum-warning-color)' }}
+                  />
+                </Flex.Item>
+                <Flex.Item>
+                  <Trans>
+                    Some wallets do not provide our interface with complete
+                    information about your ADA addresses so the reward can be
+                    displayed inaccurately. If you are sure you are eligible for
+                    any type of reward, try to recover your seed phrase using{' '}
+                    <Button
+                      type="link"
+                      target="_blank"
+                      href={Eternl.extensionLink}
+                      style={{
+                        height: 'auto',
+                        padding: 0,
+                        lineHeight: 0,
+                        display: 'inline-block',
+                      }}
+                    >
+                      Eternl
+                    </Button>{' '}
+                    wallet. If that doesn&apos;t work, contact{' '}
+                    <Button
+                      type="link"
+                      target="_blank"
+                      href={SPECTRUM_DISCORD_LINK}
+                      style={{
+                        height: 'auto',
+                        lineHeight: 0,
+                        padding: 0,
+                        display: 'inline-block',
+                      }}
+                    >
+                      support
+                    </Button>
+                    .
+                  </Trans>
+                </Flex.Item>
+              </Flex>
+            }
+          />
         </Flex.Item>
-        {applicationConfig.isRewardsAvailable ? (
-          <ConnectWalletButton
-            width="100%"
-            size="extra-large"
-            trace={{
-              element_location: ElementLocation.rewardsPage,
-              element_name: ElementName.connectWalletButton,
-            }}
-          >
-            {loading ? (
-              <Skeleton active />
-            ) : (
-              rewardsData && <RewardsDashboard rewardsData={rewardsData} />
+      )}
+      <Page width={500}>
+        <Flex col>
+          <Flex.Item marginBottom={4} display="flex" justify="space-between">
+            <Typography.Title level={4}>Rewards</Typography.Title>
+            {applicationConfig.isRewardsAvailable && (
+              <Button href={SPECTRUM_DISCORD_LINK} target="_blank">
+                Support
+              </Button>
             )}
-          </ConnectWalletButton>
-        ) : (
-          <RewardsBugFixing />
-        )}
-      </Flex>
-    </Page>
+          </Flex.Item>
+          {applicationConfig.isRewardsAvailable ? (
+            <ConnectWalletButton
+              width="100%"
+              size="extra-large"
+              trace={{
+                element_location: ElementLocation.rewardsPage,
+                element_name: ElementName.connectWalletButton,
+              }}
+            >
+              {loading ? (
+                <Skeleton active />
+              ) : (
+                rewardsData && <RewardsDashboard rewardsData={rewardsData} />
+              )}
+            </ConnectWalletButton>
+          ) : (
+            <RewardsBugFixing />
+          )}
+        </Flex>
+      </Page>
+    </Flex>
   );
 };
