0a8623ad8810bad0586d3d4ba479b3959fa36126
ridel1e <ridellee@gmail.com>
2023-11-09 16:55:27 +0300

    try to fix position observable

--- src/network/cardano/api/positions/positions.ts
+++ src/network/cardano/api/positions/positions.ts
@@ -1,6 +1,8 @@
 import {
   combineLatest,
   debounceTime,
+  filter,
+  first,
   map,
   publishReplay,
   refCount,
@@ -16,6 +18,7 @@ import {
   unverifiedAmmPools$,
 } from '../ammPools/ammPools';
 import { lpBalance$ } from '../balance/lpBalance';
+import { networkAssetBalance$ } from '../balance/networkAssetBalance.ts';
 import { networkContext$ } from '../networkContext/networkContext';
 
 export const positions$ = combineLatest([
@@ -27,7 +30,11 @@ export const positions$ = combineLatest([
       return allAmmPools$;
     }),
   ),
-  lpBalance$.pipe(startWith(new Balance([]))),
+  networkAssetBalance$.pipe(
+    filter(Boolean),
+    first(),
+    switchMap(() => lpBalance$.pipe(startWith(new Balance([])))),
+  ),
   networkContext$,
 ]).pipe(
   debounceTime(200),
