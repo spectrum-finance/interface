20aa87ab13b71638d83d190aea9d6b0c9c317810
ridel1e <ridellee@gmail.com>
2023-11-27 12:13:55 +0300

    add lbsp tag to pool info view

--- src/pages/PoolOverview/PoolInfoView/PoolInfoView.tsx
+++ src/pages/PoolOverview/PoolInfoView/PoolInfoView.tsx
@@ -23,10 +23,13 @@ import { isDeprecatedPool } from '../../../common/utils/isDeprecatedPool';
 import { ConnectWalletButton } from '../../../components/common/ConnectWalletButton/ConnectWalletButton';
 import { DeprecatedPoolTag } from '../../../components/DeprecatedPoolTag/DeprecatedPoolTag';
 import { FarmsButton } from '../../../components/FarmsButton/FarmsButton';
+import { LbspPoolTag } from '../../../components/LbspPoolTag/LbspPoolTag.tsx';
 import { PageHeader } from '../../../components/Page/PageHeader/PageHeader';
 import { redeem } from '../../../gateway/api/operations/redeem';
 import { useSelectedNetwork } from '../../../gateway/common/network';
+import { isLbspPool } from '../../../network/cardano/api/lbspWhitelist/lbspWhitelist.ts';
 import { hasFarmsForPool } from '../../../network/ergo/lm/api/farms/farms';
+import { isSpfPool } from '../../../utils/lbsp.ts';
 import { MyLiquidity } from './MyLiquidity/MyLiquidity';
 import { PoolFeeTag } from './PoolFeeTag/PoolFeeTag';
 import { TotalLiquidity } from './TotalLiquidity/TotalLiquidity';
@@ -40,6 +43,7 @@ export const PoolInfoView: FC<PoolInfoProps> = ({ position }) => {
   const navigate = useNavigate();
   const [selectedNetwork] = useSelectedNetwork();
   const [hasFarmForPool] = useObservable(hasFarmsForPool(position.pool.id), []);
+  const [_isLbspPool] = useObservable(isLbspPool(position.pool.id));
 
   const handleFarmsButtonClick = () =>
     navigate(`../../../farm?searchString=${position?.pool.id}`);
@@ -118,6 +122,9 @@ export const PoolInfoView: FC<PoolInfoProps> = ({ position }) => {
                   <Flex.Item marginRight={2}>
                     <DeprecatedPoolTag />
                   </Flex.Item>
+                )}
+                {!s && (_isLbspPool || isSpfPool(position.pool.id)) && (
+                  <LbspPoolTag isSpf={isSpfPool(position.pool.id)} />
                 )}
                 {hasFarmForPool && (
                   <FarmsButton onClick={handleFarmsButtonClick} />
