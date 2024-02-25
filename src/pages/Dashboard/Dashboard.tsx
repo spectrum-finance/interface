e7e4c7b3abfefb482729561a28c15649eb837bae
M Afzaal Afzal <afzaal1721@gmail.com>
2024-02-25 13:05:45 +0500

    feat: create basic layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,77 @@
+import styled from 'styled-components';
+
+import { RightSideCoinList } from './RightSideCoinList';
+
+const MainContainer = styled('div')`
+  padding: 0 1rem;
+`;
+
+const Container = styled('div')`
+  max-width: 1400px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: 1fr 338px;
+  gap: 2rem;
+`;
+
+const LeftSideContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const TopChartsContainer = styled('div')`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+`;
+
+const Chart = styled('div')`
+  height: 288px;
+  background: var(--teddy-box-color);
+  border-radius: 18px;
+`;
+
+const BottomChartsContainer = styled('div')`
+  display: grid;
+  grid-template-columns: 1.7fr 1fr;
+  gap: 2rem;
+`;
+
+const BottomChart = styled('div')`
+  height: 232px;
+  background: var(--teddy-box-color);
+  border-radius: 18px;
+`;
+
+const LastCardsContainer = styled('div')`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+`;
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <MainContainer>
+      <Container>
+        <LeftSideContainer>
+          <TopChartsContainer>
+            <Chart>chart</Chart>
+            <Chart>chart</Chart>
+          </TopChartsContainer>
+          <BottomChartsContainer>
+            <BottomChart>123</BottomChart>
+            <BottomChart>123</BottomChart>
+          </BottomChartsContainer>
+          <LastCardsContainer>
+            <BottomChart>123</BottomChart>
+            <BottomChart>123</BottomChart>
+          </LastCardsContainer>
+        </LeftSideContainer>
+        <RightSideCoinList />
+      </Container>
+    </MainContainer>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/TimeRangePicker.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+const Container = styled('div')`
+  padding: 2px;
+  background: linear-gradient(
+    129deg,
+    rgba(69, 78, 96, 0.63) 11.81%,
+    rgba(73, 79, 89, 0.63) 48.34%,
+    rgba(43, 56, 74, 0.63) 98.82%
+  );
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TimeItem = styled('div')<{ isActive?: boolean }>`
+  padding: 8px 12px;
+  border-radius: 10px;
+  font-size: 12px;
+  color: #ffffff;
+  font-weight: 600;
+
+  cursor: pointer;
+  ${(props) =>
+    props.isActive &&
+    `
+     background: linear-gradient(
+    315deg,
+    #181d2d -11.92%,
+    rgba(46, 47, 58, 0.75) 119.58%
+  );
+  `}
+`;
+
+const availableTimes = ['1h', '1d', '1w', '1m', '3m', '1y', 'All Time'];
+
+export type TimeType = '1w' | '1d' | '1h' | '1m' | '3m' | '1y' | 'All Time';
+
+type TimeRangePickerProps = {
+  selectedTime: TimeType;
+  setSelectedTime: (val: TimeType) => void;
+};
+
+export const TimeRangePicker = ({
+  selectedTime,
+  setSelectedTime,
+}: TimeRangePickerProps) => {
+  const updateSelectedTime = (val: TimeType) => {
+    setSelectedTime(val);
+  };
+
+  return (
+    <Container>
+      {availableTimes.map((val) => (
+        <TimeItem
+          onClick={() => {
+            updateSelectedTime(val as TimeType);
+          }}
+          isActive={val === selectedTime}
+          key={val}
+        >
+          {val}
+        </TimeItem>
+      ))}
+    </Container>
+  );
+};
