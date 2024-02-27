74f4a451ff66116b8562969a0f318cdfacbc7ceb
M Afzaal Afzal <afzaal1721@gmail.com>
2024-02-27 12:44:39 +0500

    feat: refine design

--- src/pages/Dashboard/BatchersCard/BatchersCard.tsx
+++ src/pages/Dashboard/BatchersCard/BatchersCard.tsx
@@ -1,14 +1,33 @@
-import { ReactComponent } from '../../../assets/icons/icon-sun-blue.svg';
+import { ElementType } from 'react';
+
+import { ReactComponent as MilitaryTech } from '../../../assets/icons/icon-military-tech.svg';
+import { ReactComponent as SunBlue } from '../../../assets/icons/icon-sun-blue.svg';
+import { ReactComponent as WorldBlue } from '../../../assets/icons/icon-world-blue.svg';
 import { LeftTextWithRightSunText } from '../LeftTextWithRightSunText/LeftTextWithRightSunText';
 import styles from './BachersCard.module.less';
 
-const SingleItem = () => {
+type SingleItemProps = {
+  title: string;
+  subTitle: string;
+  icon: {
+    element: ElementType;
+    width: number;
+    height: number;
+  };
+};
+
+const SingleItem = ({ title, subTitle, icon }: SingleItemProps) => {
+  const IconComponent = icon.element;
   return (
     <div className={styles.singleItem}>
-      <ReactComponent className={styles.image} />
+      <IconComponent
+        width={icon.width}
+        className={styles.image}
+        height={icon.height}
+      />
       <div className={styles.rightSection}>
-        <div className={styles.title}>₳3,303,930.12</div>
-        <div className={styles.subTitle}>Distributed to Batchers</div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subTitle}>{subTitle}</div>
       </div>
     </div>
   );
@@ -23,9 +42,22 @@ export const BatchersCard = () => {
       />
       <div className={styles.bottomSection}>
         <div className={styles.itemsList}>
-          <SingleItem />
-          <SingleItem />
-          <SingleItem />
+          <SingleItem
+            title="₳3,303,930.12"
+            subTitle="Distributed to Batchers"
+            icon={{ element: SunBlue, height: 29, width: 29 }}
+          />
+          <SingleItem
+            title="236"
+            subTitle="Batchers Worldwide"
+            icon={{ element: WorldBlue, height: 29, width: 29 }}
+          />
+          <SingleItem
+            title="1st"
+            subTitle="Open-Source batcher 
+protocol on Cardano"
+            icon={{ element: MilitaryTech, height: 36, width: 29 }}
+          />
         </div>
         <div className={styles.btnList}>
           <button className={styles.btnPrimary}>Run a Batcher</button>
